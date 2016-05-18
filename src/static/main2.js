6cbfb1ead17bdafd12303f20acf78e8cb680162d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-05-18 13:43:15 +0200

    Continue development

--- src/static/main2.js
+++ src/static/main2.js
@@ -54,6 +54,7 @@ window.Instant = function() {
   }
   /* Nick-name handling */
   Instant.nick = function() {
+    /* Nick -> Hue hash */
     var cache = {};
     return {
       /* Normalize nick-name for fuzzy matching and @-mentions */
@@ -88,6 +89,56 @@ window.Instant = function() {
         node.style.background = 'hsl(' + hue + ', 75%, 80%)';
         return node;
       }
+    };
+  }();
+  /* Message handling */
+  Instant.message = function() {
+    /* Message ID -> DOM node */
+    var nodes = {};
+    /* Locale-agnostic abbreviated month name table */
+    var MONTH_NAMES = { 1: 'Jan',  2: 'Feb',  3: 'Mar',  4: 'Apr',
+                        5: 'May',  6: 'Jun',  7: 'Jul',  8: 'Aug',
+                        9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'};
+    /* Interesting substring regex.
+     * Groupings:  1: Room name matched
+     *             2: Full URL matched
+     *                3: Scheme (with colon and double slash)
+     *                4: Username with "@"
+     *                5: Hostname
+     *                6: Port
+     *                7: Path
+     *             8: Nick-name @-mentioned
+     *             9: Smiley (space before must be ensured)
+     *            10: Text with highlighting marker(s) (check space before)
+     *                11: Markers before and after
+     *                12: Marker before
+     *                13: Marker after
+     */
+    var mc = /[^.,:;!?()\s]/;
+    var ALLOW_AROUND = /[^a-zA-Z0-9_]|^|$/;
+    var INTERESTING = (
+      '\\B&([a-zA-Z](?:[a-zA-Z0-9_-]*[a-zA-Z0-9])?)\\b|' +
+      '<(((?!javascript:)[a-zA-Z]+://)?([a-zA-Z0-9._~-]+@)?' +
+        '([a-zA-Z0-9.-]+)(:[0-9]+)?(/[^>]*)?)>|' +
+      '\\B@(%%MC%%+(?:\\(%MC%*\\)%MC%*)*)|' +
+      '((?:[+-]1|:[D)|/(CSP\\\\oO]|[SD)/|(C\\\\oO]:|\\^\\^|;\\))(?=%AA%))|' +
+      '((?:\\*+([^*\\s]+)\\*+|\\*+([^*\\s]+)|([^*\\s]+)\\*+)(?=%AA%))'
+      ).replace(/%MC%/g, mc).replace(/%AA%/g, ALLOW_AROUND);
+    var SMILEYS = {'+1': '#008000', '-1': '#c00000',
+      ':D': '#c0c000', ':)': '#c0c000', ':|': '#c0c000', ':/': '#c0c000',
+      ':(': '#c0c000', ':C': '#c0c000', ':S': '#c0c000', ':P': '#c0c000',
+      'S:': '#c0c000', 'D:': '#c0c000', '):': '#c0c000', '/:': '#c0c000',
+      '|:': '#c0c000', '(:': '#c0c000', 'C:': '#c0c000',
+      ':O': '#c0c000', ':o': '#c0c000', 'o:': '#c0c000', 'O:': '#c0c000',
+      ';)': '#c0c000', '^^': '#c0c000',
+      ':\\': '#c0c000', '\\:': '#c0c000'
+    };
+    return {
+      /* Detect links, emphasis, and smileys out of a flat string and
+       * render those into a DOM node */
+      parseContent: function(text) {
+        /**/
+      };
     };
   }();
   /* To be assigned in window */
