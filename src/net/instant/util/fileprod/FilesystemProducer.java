56e747c71213464c1cc9f0090c83259260870f77
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-06-22 23:37:07 +0200

    [Backend] Expand FilesystemProducer

--- src/net/instant/util/fileprod/FilesystemProducer.java
+++ src/net/instant/util/fileprod/FilesystemProducer.java
@@ -6,8 +6,41 @@ import java.io.InputStream;
 
 public class FilesystemProducer implements Producer {
 
+    private File chroot;
+    private File workdir;
+
+    public FilesystemProducer(File chroot, File workdir) {
+        this.chroot = chroot;
+        this.workdir = workdir;
+    }
+    public FilesystemProducer(String chroot, String workdir) {
+        this(new File(chroot), new File(workdir));
+    }
+    public FilesystemProducer() {
+        this("/", "");
+    }
+
+    public File getChroot() {
+        return chroot;
+    }
+    public File getWorkdir() {
+        return workdir;
+    }
+
+    public void setChroot(File f) {
+        chroot = f;
+    }
+    public void setWorkdir(File f) {
+        workdir = f;
+    }
+
+    public File convert(String name) {
+        String adjustedPath = new File(workdir, name).toString();
+        return new File(chroot, adjustedPath.replaceFirst("^[/\\\\]+", ""));
+    }
+
     public ProducerJob produce(String name) {
-        final File path = new File(name.replaceFirst("^[/\\\\]+", ""));
+        final File path = convert(name);
         if (! path.isFile()) return null;
         return new ProducerJob(name) {
             protected FileCell produce() throws IOException {
