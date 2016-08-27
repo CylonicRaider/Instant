70a5af0c069505261dee57a950dd9d28ce4983b5
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-08-27 20:41:21 +0200

    [API] [Backend] Clarify FileInfo docs

--- src/net/instant/api/FileInfo.java
+++ src/net/instant/api/FileInfo.java
@@ -4,24 +4,27 @@ import java.nio.ByteBuffer;
 
 /**
  * Information about a static file and its contents.
+ * The methods marked as "assumed to be constant" may not be called but once
+ * for filling internal data structures by the core.
  */
 public interface FileInfo {
 
     /**
      * The name of the file.
-     * Used for caching purposes.
+     * Used for caching purposes. Is assumed to be constant.
      */
     String getName();
 
     /**
      * The (binary) content of the file.
      * If the file is textual, a UTF-8 encoding is silently assumed.
-     * May be read-only.
+     * May be read-only. The reference is assumed to be constant.
      */
     ByteBuffer getData();
 
     /**
      * The creation date of the (cached) file as a UNIX timestamp.
+     * Is assumed to be constant.
      */
     long getCreated();
 
