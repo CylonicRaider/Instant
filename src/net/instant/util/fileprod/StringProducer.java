5178724b2b7c44a22ff990a7e37b1f3092bde3d8
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-09-01 12:25:50 +0200

    [Backend] Refactor StringProducer
    Should support post-production updates now.

--- src/net/instant/util/fileprod/StringProducer.java
+++ src/net/instant/util/fileprod/StringProducer.java
@@ -2,57 +2,102 @@ package net.instant.util.fileprod;
 
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import net.instant.util.Util;
 
 public class StringProducer implements Producer {
 
+    public class StringFileCell extends FileCell {
+
+        public StringFileCell(String name, ByteBuffer content,
+                              long created) {
+            super(name, content, created);
+        }
+
+        public boolean isValid() {
+            synchronized (StringProducer.this) {
+                return (lastUpdates.get(getName()) == getCreated());
+            }
+        }
+
+    }
+
     private final Map<String, ByteBuffer> files;
+    private final Map<String, Long> lastUpdates;
+    private final Map<String, List<ByteBuffer>> pendingAppends;
 
     public StringProducer() {
         files = new HashMap<String, ByteBuffer>();
+        lastUpdates = new HashMap<String, Long>();
+        pendingAppends = new HashMap<String, List<ByteBuffer>>();
     }
 
-    public synchronized void addFile(String name, ByteBuffer content) {
+    protected synchronized void addFileEx(String name, ByteBuffer content) {
         files.put(name, content.asReadOnlyBuffer());
+    }
+    public synchronized void addFile(String name, ByteBuffer content) {
+        addFileEx(name, content);
+        pendingAppends.remove(name);
+        lastUpdates.put(name, System.currentTimeMillis());
     }
     public void addFile(String name, String content) {
         addFile(name, Util.toBytes(content));
     }
     public synchronized void appendFile(String name, ByteBuffer content) {
-        ByteBuffer oldContent = getFile(name);
+        ByteBuffer oldContent = getCachedFile(name);
         if (oldContent == null) {
             addFile(name, content);
             return;
         }
-        ByteBuffer newContent = ByteBuffer.allocate(oldContent.limit() +
-            content.limit());
-        newContent.put(oldContent);
-        newContent.put(content);
-        newContent.flip();
-        addFile(name, newContent);
+        List<ByteBuffer> queue = pendingAppends.get(name);
+        if (queue == null) {
+            queue = new LinkedList<ByteBuffer>();
+            pendingAppends.put(name, queue);
+        }
+        queue.add(content);
+        lastUpdates.put(name, System.currentTimeMillis());
     }
     public void appendFile(String name, String content) {
         appendFile(name, Util.toBytes(content));
     }
     public synchronized void removeFile(String name) {
         files.remove(name);
+        pendingAppends.remove(name);
     }
 
     public synchronized String[] listFiles() {
         return files.keySet().toArray(new String[0]);
     }
-    public synchronized ByteBuffer getFile(String name) {
+    public synchronized ByteBuffer getCachedFile(String name) {
         return files.get(name);
+    }
+    public synchronized ByteBuffer getFile(String name) {
+        ByteBuffer oldData = getCachedFile(name);
+        List<ByteBuffer> queue = pendingAppends.remove(name);
+        if (queue == null) return oldData;
+        int len = oldData.limit();
+        for (ByteBuffer b : queue) len += b.limit();
+        ByteBuffer newData = ByteBuffer.allocate(len);
+        newData.put(oldData);
+        for (ByteBuffer b : queue) newData.put(b);
+        newData.flip();
+        addFile(name, newData);
+        return newData.asReadOnlyBuffer();
     }
 
     public ProducerJob produce(String name) {
-        final long pollTime = System.currentTimeMillis();
-        final ByteBuffer content = files.get(name);
+        final ByteBuffer content;
+        final Long pollTime;
+        synchronized (this) {
+            content = getFile(name);
+            pollTime = lastUpdates.get(name);
+        }
         if (content == null) return null;
         return new ProducerJob(name) {
             protected FileCell produce() {
-                return new FileCell(getName(), content, pollTime);
+                return new StringFileCell(getName(), content, pollTime);
             }
         };
     }
