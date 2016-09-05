24501738f3052fc52910186712e8ffe82526fc29
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-09-05 19:33:36 +0200

    [Backend] Implement plugin fetching

--- src/net/instant/plugins/PluginManager.java
+++ src/net/instant/plugins/PluginManager.java
@@ -1,14 +1,23 @@
 package net.instant.plugins;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PluginManager {
 
     private final Map<String, Plugin> plugins;
+    private PluginFetcher fetcher;
 
     public PluginManager() {
         plugins = new LinkedHashMap<String, Plugin>();
+    }
+
+    public PluginFetcher getFetcher() {
+        return fetcher;
+    }
+    public void setFetcher(PluginFetcher f) {
+        fetcher = f;
     }
 
     public Plugin get(String name) {
@@ -16,6 +25,16 @@ public class PluginManager {
     }
     public void add(Plugin p) {
         plugins.put(p.getName(), p);
+    }
+
+    public Plugin fetch(String name)
+            throws IOException, NoSuchPluginException {
+        Plugin p = get(name);
+        if (p != null) return p;
+        p = fetcher.fetch(this, name);
+        if (p == null) throw new NoSuchPluginException(name);
+        add(p);
+        return p;
     }
 
 }
