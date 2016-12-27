CylonicRaider/Instant

commit 5425923d6b045cd3ae9c33679545b7efd54130cd

[Frontend] Refactor sidebar

src/static/main.js
@@ -2028,20 +2028,8 @@ this.Instant = function() {
           var n = node.children.length.toString();
           c.textContent = n + ' user' + ((/(^|[^1])1$/.test(n)) ? '' : 's');
         }
-        /* Determine if the list is wrapped; cannot do anything if not */
-        var par = node.parentNode;
-        if (! par || ! par.classList.contains('user-list-wrapper'))
-          return;
-        /* Make measurements accurate */
-        par.style.minWidth = '';
-        /* HACK to check for scrollbar :P */
-        if (par.clientWidth != par.offsetWidth) {
-          par.classList.add('overflow');
-          par.style.minWidth = par.offsetWidth + (par.offsetWidth -
-            par.clientWidth) + 'px';
-        } else {
-          par.classList.remove('overflow');
-        }
+        /* Now delegated to sidebar itself */
+        Instant.animation.updateSidebar();
       },
       /* Collapse or uncollapse the user list */
       collapse: function(invisible) {
@@ -2762,9 +2750,11 @@ this.Instant = function() {
   Instant.animation = function() {
     /* The main message box */
     var messageBox = null;
+    /* The sidebar */
+    var sideBar = null;
     return {
       /* Initialize the submodule */
-      init: function(node) {
+      init: function(msgNode, barNode) {
         function updateHash(event) {
           if (/^#?$/.test(location.hash)) {
             Instant.input.navigate('root');
@@ -2774,7 +2764,11 @@ this.Instant = function() {
             Instant.animation.navigateToMessage(location.hash);
           }
         }
-        messageBox = node;
+        function mutationInvalid(el) {
+          return (el.target == wrapper);
+        }
+        messageBox = msgNode;
+        sideBar = barNode;
         var pane = Instant.pane.getPane(messageBox);
         /* Pull more logs when scrolled to top */
         pane.addEventListener('scroll', function(event) {
@@ -2783,6 +2777,16 @@ this.Instant = function() {
         });
         window.addEventListener('hashchange', updateHash);
         updateHash();
+        /* Sidebar handling */
+        var wrapper = $sel('.sidebar-middle-wrapper', sideBar);
+        window.addEventListener('resize', Instant.animation.updateSidebar);
+        var obs = new MutationObserver(function(records, observer) {
+          if (records.some(mutationInvalid)) return;
+          Instant.animation.updateSidebar();
+        });
+        obs.observe(wrapper, {childList: true, attributes: true,
+          characterData: true, subtree: true,
+          attributeFilter: ['class', 'style']});
       },
       /* Navigate the input to the given message */
       goToMessage: function(msg) {
@@ -2813,6 +2817,22 @@ this.Instant = function() {
         var msg = $sel('.message', messageBox);
         if (msg && msg.offsetTop >= tp && pane.scrollTop == 0)
           Instant.logs.pull.more();
+      },
+      /* Update sidebar styles */
+      updateSidebar: function() {
+        /* Extract nodes */
+        var wrapper = $sel('.sidebar-middle-wrapper', sideBar);
+        var content = $sel('.sidebar-middle', wrapper);
+        /* Make measurements accurate */
+        wrapper.style.minWidth = '';
+        /* HACK to check for the presence of (explicit) scrollbars */
+        if (wrapper.clientWidth != wrapper.offsetWidth) {
+          wrapper.classList.add('overflow');
+          wrapper.style.minWidth = wrapper.offsetWidth +
+            (wrapper.offsetWidth - wrapper.clientWidth) + 'px';
+        } else {
+          wrapper.classList.remove('overflow');
+        }
       },
       /* Greeting pane */
       greeter: function() {

src/static/room.css
@@ -224,11 +224,12 @@ body {
   flex-direction: column;
   align-items: flex-end;
   min-height: 0;
-  pointer-events: auto;
+  pointer-events: none;
 }
 .sidebar-top > *, .sidebar-middle > *, .sidebar-bottom > * {
   background: rgba(255, 255, 255, 0.5);
   padding: 3px 0 3px 3px;
+  pointer-events: auto;
 }
 .sidebar-top, .sidebar-bottom {
   -webkit-flex-shrink: 0;
@@ -239,10 +240,15 @@ body {
   line-height: 1.2;
   padding: 5px 0;
 }
-
+.sidebar-middle-wrapper {
+  padding: 0;
+  overflow: auto;
+}
+.sidebar-middle-wrapper.overflow {
+  padding-right: 3px;
+}
 .sidebar-middle {
-  -webkit-flex-shrink: 1;
-  flex-shrink: 1;
+  pointer-events: none;
 }
 
 .throbber {
@@ -380,14 +386,6 @@ body {
   font-weight: bold;
 }
 
-/* WARNING: Must have no horizontal margin or padding to avoid scrollbar
- *          insanities */
-.user-list-wrapper {
-  padding-left: 0;
-  max-height: 75vh;
-  overflow: auto;
-  background: none;
-}
 .user-list {
   display: -webkit-flex;
   display: flex;
@@ -395,15 +393,14 @@ body {
   -webkit-align-items: flex-end;
   flex-direction: column;
   align-items: flex-end;
+  background: none;
+  pointer-events: none;
 }
-.user-list-wrapper > .user-list {
-  padding-left: 3px;
-}
-.user-list-wrapper.overflow > .user-list {
-  padding-right: 3px;
-}
-.user-list.collapsed, .user-list-wrapper.collapsed {
+.user-list.collapsed {
   display: none;
+}
+.user-list > * {
+  pointer-events: auto;
 }
 
 .user-list .nick {

src/static/room.html
@@ -94,9 +94,10 @@
               outdated;<br/>
               please refresh it manually</div>
           </div>
-          <div class="sidebar-middle">
-            <div class="user-list-wrapper"><div
-              class="user-list"></div></div>
+          <div class="sidebar-middle-wrapper">
+            <div class="sidebar-middle">
+              <div class="user-list"></div>
+            </div>
           </div>
           <div class="sidebar-bottom">
             <div class="user-list-counter"><a href="#">

commit 5d4a8dc1a8d8730462267c5a5010aba17fccff2a

[Frontend] Improve compatibility
Issue introduced in commit 5425923d6b045cd3ae9c33679545b7efd54130cd.

src/static/main.js
@@ -2808,13 +2808,15 @@ this.Instant = function() {
         /* Sidebar handling */
         var wrapper = $sel('.sidebar-middle-wrapper', sideBar);
         window.addEventListener('resize', Instant.animation.updateSidebar);
-        var obs = new MutationObserver(function(records, observer) {
-          if (records.some(mutationInvalid)) return;
-          Instant.animation.updateSidebar();
-        });
-        obs.observe(wrapper, {childList: true, attributes: true,
-          characterData: true, subtree: true,
-          attributeFilter: ['class', 'style']});
+        if (window.MutationObserver) {
+          var obs = new MutationObserver(function(records, observer) {
+            if (records.some(mutationInvalid)) return;
+            Instant.animation.updateSidebar();
+          });
+          obs.observe(wrapper, {childList: true, attributes: true,
+            characterData: true, subtree: true,
+            attributeFilter: ['class', 'style']});
+        }
       },
       /* Navigate the input to the given message */
       goToMessage: function(msg) {