a631b4bcc3c469042f3cab9987a47952baaedec2
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-08-02 19:39:33 +0200

    [Backend] Log cleanup of connections

--- src/net/instant/ws/ConnectionGC.java
+++ src/net/instant/ws/ConnectionGC.java
@@ -3,9 +3,12 @@ package net.instant.ws;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 import net.instant.api.RequestResponseData;
 
 public class ConnectionGC implements Runnable {
+
+    private static final Logger LOGGER = Logger.getLogger("ConnGC");
 
     public static final long INTERVAL = 10000;
     public static final long GRACE_TIME = 5000;
@@ -41,13 +44,19 @@ public class ConnectionGC implements Runnable {
         deadlines.remove(connection);
     }
 
+    public void cleanup(RequestResponseData r) {
+        LOGGER.info("Cleaned up connection (" + r.getConnection() + ")");
+        r.getConnection().close();
+    }
+
     public void run() {
         for (;;) {
             long now = System.currentTimeMillis();
             for (Map.Entry<RequestResponseData, Long> e :
                  getDeadlines().entrySet()) {
-                if (e.getValue() != null && e.getValue() < now)
-                    e.getKey().getConnection().close();
+                if (e.getValue() != null && e.getValue() < now) {
+                    cleanup(e.getKey());
+                }
             }
             try {
                 Thread.sleep(INTERVAL);
