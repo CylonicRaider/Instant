a1ad2f8711cde5c48d4f8620b18af355e7f48db9
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-08-16 23:14:23 +0200

    [Makefile] [Linter] Correct various issues

--- script/importlint.py
+++ script/importlint.py
@@ -4,12 +4,19 @@
 import sys, os, re
 
 RAW_IDENT = r'[a-zA-Z_$][a-zA-Z0-9_$]*'
+KEYWORDS = ('abstract|assert|boolean|break|byte|case|catch|char|class|const|'
+    'continue|default|do|double|else|enum|extends|final|finally|float|for|'
+    'goto|if|implements|import|instanceof|int|interface|long|native|new|'
+    'package|private|protected|public|return|short|static|strictfp|super|'
+    'switch|synchronized|this|throw|throws|transient|try|void|volatile|'
+    'while')
 REGEXES = {
     # Identifier. Not matching the "e" in float literals.
     # FIXME: Unicode support.
-    'identifier': re.compile(r'(?!<[a-zA-Z0-9_$])' + RAW_IDENT),
+    'identifier': re.compile(r'(?!%s)(?<![a-zA-Z0-9_$])%s' %
+                             (KEYWORDS, RAW_IDENT)),
     # Interesting statement.
-    'import': re.compile(r'(?!<[a-zA-Z0-9_$])import\s+(static\s+)?'
+    'import': re.compile(r'(?<![a-zA-Z0-9_$])import\s+(static\s+)?'
         r'(?P<name>%(i)s\s*(\.\s*%(i)s\s*)*);' % {'i': RAW_IDENT}),
     # Character or string. (The code is supposed to be syntactically valid.)
     'charstring': re.compile(r'''(?s)'([^']|\.)+'|"([^"]|\.)*"'''),
@@ -27,7 +34,7 @@ def regex_race(data, regexes):
     pos, hits = 0, dict((k, None) for k in regexes)
     while pos != len(data):
         # Update regex matches.
-        for k, v in hits.items():
+        for k, v in tuple(hits.items()):
             if v is None or v.start() < pos:
                 m = REGEXES[k].search(data, pos)
                 if m:
@@ -38,10 +45,14 @@ def regex_race(data, regexes):
         if not hits: break
         # Find first match.
         first = min(hits.values(), key=lambda m: m.start())
-        # Emit it.
+        # Emit preceding data and the match.
+        if first.start() != pos: yield data[pos:first.start()]
         yield first
         # Prepare for next round.
         pos = first.end()
+    # Emit remainder.
+    if pos != len(data):
+        yield data[pos:]
 
 def main():
     """
