1959a4a3a5a345a3ba4871c1eeb9751c083ed4a3
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-08-25 21:04:41 +0200

    [Backend] Commence large-scale refactoring

--- src/net/instant/Main.java
+++ src/net/instant/Main.java
@@ -1,35 +1,12 @@
 package net.instant;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.net.URL;
 import java.util.jar.Manifest;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
-import net.instant.hooks.Error404Hook;
-import net.instant.plugins.PluginException;
 import net.instant.util.Formats;
 import net.instant.util.Logging;
-import net.instant.util.StringSigner;
-import net.instant.util.Util;
-import net.instant.util.argparse.ActionOption;
-import net.instant.util.argparse.ArgParser;
-import net.instant.util.argparse.IntegerOption;
-import net.instant.util.argparse.ParseException;
-import net.instant.util.argparse.ParseResult;
-import net.instant.util.argparse.PathListOption;
-import net.instant.util.argparse.StringListOption;
-import net.instant.util.argparse.StringOption;
-import net.instant.util.fileprod.FileProducer;
-import net.instant.util.fileprod.FilesystemProducer;
-import net.instant.util.fileprod.ResourceProducer;
-import net.instant.ws.CookieHandler;
-import net.instant.ws.InstantWebSocketServer;
 
 public class Main implements Runnable {
 
@@ -67,13 +44,9 @@ public class Main implements Runnable {
     }
 
     private final String[] args;
-    private InstantRunner runner;
-    private String startupCmd;
 
     public Main(String[] args) {
         this.args = args;
-        this.runner = new InstantRunner();
-        this.startupCmd = null;
     }
 
     public int getArguments() {
@@ -86,157 +59,8 @@ public class Main implements Runnable {
         return args[i];
     }
 
-    public InstantRunner getRunner() {
-        return runner;
-    }
-    public void setRunner(InstantRunner r) {
-        runner = r;
-    }
-
-    protected void parseArguments() {
-        ArgParser p = new ArgParser("Instant");
-        p.addHelp();
-        p.add(new ActionOption("version") {
-            public void run() {
-                String output = "Instant v" + VERSION;
-                if (FINE_VERSION != null)
-                    output += " (" + FINE_VERSION + ")";
-                System.err.println(output);
-                System.exit(0);
-            }
-        }, "Show the current version");
-        StringOption host = p.add(new StringOption("host", false, "*"),
-            "Host to bind to");
-        IntegerOption port = p.add(new IntegerOption("port", true, 8080),
-            "Port number to use");
-        StringOption httplog = p.add(new StringOption("http-log", false,
-            "-"), "Log file for HTTP requests");
-        StringOption dbglog = p.add(new StringOption("debug-log", false,
-            "-"), "Log file for debugging");
-        StringOption loglevel = p.add(new StringOption("log-level", false,
-           "INFO"), "Logging level");
-        PathListOption pluginPath = p.add(new PathListOption(
-            "plugin-path", false), "Path to search for plugins in");
-        StringListOption plugins = p.add(new StringListOption(
-            "plugins", false), "List of plugins to load");
-        StringOption cmdOpt = p.add(new StringOption("startup-cmd", false,
-            null), "A shell command to run just before entering the " +
-            "main loop");
-        ParseResult r;
-        try {
-            r = p.parse(args);
-        } catch (ParseException exc) {
-            System.err.println(exc.getMessage());
-            System.exit(1);
-            return;
-        }
-        String hostName = host.get(r);
-        if (hostName.equals("*")) hostName = null;
-        runner.setHost(hostName);
-        runner.setPort(port.get(r));
-        runner.setHTTPLog(resolveOutputStream(httplog.get(r)));
-        Logging.redirectToStream(resolveOutputStream(dbglog.get(r)));
-        Logging.setLevel(Level.parse(loglevel.get(r)));
-        for (File path : pluginPath.get(r))
-            runner.addPluginPath(path);
-        for (String plugin : plugins.get(r))
-            runner.addPlugin(plugin);
-        startupCmd = cmdOpt.get(r);
-    }
-
     public void run() {
-        parseArguments();
-        LOGGER.info("Preparing");
-        try {
-            runner.makePluginManager().load();
-        } catch (IOException exc) {
-            exc.printStackTrace();
-            System.exit(1);
-            return;
-        } catch (PluginException exc) {
-            exc.printStackTrace();
-            System.exit(1);
-            return;
-        }
-        String signaturePath = Util.getConfiguration(
-            "instant.cookies.keyfile");
-        StringSigner signer = null;
-        if (signaturePath != null)
-            signer = StringSigner.getInstance(new File(signaturePath));
-        if (signer == null)
-            signer = StringSigner.getInstance(Util.getRandomness(64));
-        runner.addFileAlias("/", "/pages/main.html");
-        runner.addFileAlias("/favicon.ico",
-                            "/static/logo-static_128x128.ico");
-        runner.addFileAlias(Pattern.compile("/([^/]+\\.html)"),
-                            "/pages/\\1");
-        runner.addFileAlias(Pattern.compile("/room/" + ROOM_RE + "/"),
-                            "/static/room.html");
-        runner.addFileAlias(Pattern.compile("/(" + STAGING_RE + ")/"),
-                            "/static/\\1/main.html");
-        runner.addFileAlias(Pattern.compile("/(" + STAGING_RE + ")/room/" +
-                            ROOM_RE + "/"), "/static/\\1/room.html");
-        runner.addContentType(".*\\.html", "text/html; charset=utf-8");
-        runner.addContentType(".*\\.css", "text/css; charset=utf-8");
-        runner.addContentType(".*\\.js", "application/javascript; " +
-                               "charset=utf-8");
-        runner.addContentType(".*\\.svg", "image/svg+xml; charset=utf-8");
-        runner.addContentType(".*\\.png", "image/png");
-        runner.addContentType(".*\\.ico", "image/vnd.microsoft.icon");
-        runner.addRedirect(Pattern.compile("/room/(" + ROOM_RE + ")"),
-                           "/room/\\1/", 301);
-        runner.addRedirect(Pattern.compile("/(" + STAGING_RE + ")"), "/\\1/",
-                           301);
-        runner.addRedirect(Pattern.compile("/(" + STAGING_RE + ")/room/(" +
-                           ROOM_RE + ")"), "/\\1/room/\\2/", 301);
-        InstantWebSocketServer srv = runner.make();
-        srv.setCookieHandler(new CookieHandler(signer));
-        FileProducer prod = runner.getFileHook().getProducer();
-        FilesystemProducer fp = new FilesystemProducer(".", "");
-        ResourceProducer rp = new ResourceProducer(
-            runner.getPluginManager().getClassLoader());
-        fp.whitelist("/static/.*");
-        fp.whitelist("/pages/.*");
-        rp.whitelist("/static/.*");
-        rp.whitelist("/pages/.*");
-        prod.addProducer(fp);
-        prod.addProducer(rp);
-        runner.getStringProducer().addFile("/static/version.js",
-                                           VERSION_FILE);
-        srv.addInternalHook(new Error404Hook());
-        if (startupCmd != null) runCommand(startupCmd);
-        LOGGER.info("Running");
-        srv.start();
-    }
-
-    private static PrintStream resolveOutputStream(String path) {
-        if (path == null || path.equals("-")) {
-            return System.err;
-        } else {
-            try {
-                return new PrintStream(new FileOutputStream(path, true),
-                                       true);
-            } catch (FileNotFoundException exc) {
-                // Should not happen.
-                throw new RuntimeException(exc);
-            }
-        }
-    }
-
-    private static int runCommand(String cmdline) {
-        ProcessBuilder pb = new ProcessBuilder(cmdline.trim().split("\\s"));
-        pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
-        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-        try {
-            Process p = pb.start();
-            return p.waitFor();
-        } catch (IOException exc) {
-            return Integer.MIN_VALUE;
-        } catch (InterruptedException exc) {
-            Thread.currentThread().interrupt();
-            return Integer.MIN_VALUE + 1;
-        }
+        /* NYI */
     }
 
 }
