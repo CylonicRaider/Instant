f1fb2e2ece2437119d22e74a10a39c36400be1de
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-08-27 19:21:37 +0200

    [Backend] Make Option properties writable; add fluent setters

--- src/net/instant/util/argparse/Option.java
+++ src/net/instant/util/argparse/Option.java
@@ -2,8 +2,8 @@ package net.instant.util.argparse;
 
 public abstract class Option<X> {
 
-    private final String name;
-    private final String help;
+    private String name;
+    private String help;
 
     public Option(String name, String help) {
         this.name = name;
@@ -13,9 +13,23 @@ public abstract class Option<X> {
     public String getName() {
         return name;
     }
+    public void setName(String n) {
+        name = n;
+    }
+    public Option<X> name(String n) {
+        name = n;
+        return this;
+    }
 
     public String getHelp() {
         return help;
+    }
+    public void setHelp(String h) {
+        help = h;
+    }
+    public Option<X> help(String h) {
+        help = h;
+        return this;
     }
 
 }
