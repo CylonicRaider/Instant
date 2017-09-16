61e71dce849ca30a9420fa32250cb023e80593df
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-09-16 20:23:48 +0200

    [Backend] Commence PluginManager class

---
+++ src/net/instant/plugins/PluginFetcher.java
@@ -0,0 +1,58 @@
+package net.instant.plugins;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PluginFetcher {
+
+    private final List<File> singlePath;
+    private final List<File> dirPath;
+
+    public PluginFetcher() {
+        singlePath = new ArrayList<File>();
+        dirPath = new ArrayList<File>();
+    }
+
+    public void addPath(File ent) {
+        if (ent.isDirectory()) {
+            dirPath.add(ent);
+        } else if (ent.isFile()) {
+            singlePath.add(ent);
+        }
+    }
+
+    public File getPath(String name) {
+        if (name.contains("/")) {
+            File ret = new File(name);
+            if (! ret.isFile()) return null;
+            return ret;
+        }
+        for (File p : singlePath) if (matches(name, p)) return p;
+        for (File p : dirPath) {
+            for (File f : p.listFiles()) if (matches(name, f)) return f;
+        }
+        return null;
+    }
+
+    public Plugin fetch(String name) throws BadPluginException, IOException {
+        File path = getPath(name);
+        if (path == null) return null;
+        if (name.contains("/")) name = getName(name);
+        return new Plugin(name, path);
+    }
+
+    public static String getName(String path) {
+        String r = path.split("_", 2)[0];
+        if (r.endsWith(".jar")) r = r.substring(0, r.length() - 4);
+        return r;
+    }
+    public static String getName(File path) {
+        return getName(path.getName());
+    }
+    public static boolean matches(String name, File path) {
+        return (getName(path).equals(name) && path.isFile());
+    }
+
+}
---
+++ src/net/instant/plugins/PluginManager.java
@@ -0,0 +1,44 @@
+package net.instant.plugins;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PluginManager {
+
+    private final Map<String, Plugin> plugins;
+    private PluginFetcher fetcher;
+
+    public PluginManager() {
+        plugins = new LinkedHashMap<String, Plugin>();
+        fetcher = new PluginFetcher();
+    }
+
+    public PluginFetcher getFetcher() {
+        return fetcher;
+    }
+    public void setFetcher(PluginFetcher f) {
+        fetcher = f;
+    }
+
+    public Collection<Plugin> getAll() {
+        return Collections.unmodifiableCollection(plugins.values());
+    }
+    public Plugin getRaw(String name) {
+        return plugins.get(name);
+    }
+    public Plugin get(String name) throws BadPluginException, IOException {
+        Plugin ret = plugins.get(name);
+        if (ret == null && fetcher != null) {
+            ret = fetcher.fetch(name);
+            plugins.put(name, ret);
+        }
+        return ret;
+    }
+    public void add(Plugin p) {
+        plugins.put(p.getName(), p);
+    }
+
+}
