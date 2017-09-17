507bacd578aac180340277e66ca90bc1c1c0a62e
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-09-17 17:34:25 +0200

    [Backend] Order plugins correctly

--- src/net/instant/plugins/PluginManager.java
+++ src/net/instant/plugins/PluginManager.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import net.instant.util.Util;
 
 public class PluginManager {
 
@@ -55,36 +57,43 @@ public class PluginManager {
     }
     public List<Plugin> computeOrder() throws IntegrityException {
         if (order == null) {
-            checkIntegrity();
+            Map<Plugin, Set<Plugin>> deps = depencyMap();
             Set<Plugin> tempOrder = new LinkedHashSet<Plugin>();
             Deque<Plugin> stack = new LinkedList<Plugin>();
             for (Plugin p : plugins.values())
-                traversePlugins(p, stack, tempOrder);
+                traversePlugins(p, deps, stack, tempOrder);
             order = Collections.unmodifiableList(new ArrayList<Plugin>(
                 tempOrder));
         }
         return order;
     }
-    private void traversePlugins(Plugin base, Deque<Plugin> stack,
-            Set<Plugin> drain) throws IntegrityException {
-        if (base == stack.peekFirst())
-            throw new IntegrityException("Plugin " + base.getName() +
-                " depends (transitively?) on itself");
-        if (drain.contains(base))
-            return;
-        stack.add(base);
-        for (String n : base.getRequirements())
-            traversePlugins(get(n), stack, drain);
-        drain.add(base);
-        stack.remove(base);
-    }
 
-    public void checkIntegrity() throws IntegrityException {
+    private Set<Plugin> getDeps(Map<Plugin, Set<Plugin>> data, Plugin key) {
+        Set<Plugin> ret = data.get(key);
+        if (ret == null) {
+            ret = new LinkedHashSet<Plugin>();
+            data.put(key, ret);
+        }
+        return ret;
+    }
+    private Map<Plugin, Set<Plugin>> depencyMap() throws IntegrityException {
+        Map<Plugin, Set<Plugin>> ret = new HashMap<Plugin, Set<Plugin>>();
         for (Plugin p : plugins.values()) {
+            Set<Plugin> deps = getDeps(ret, p);
             for (String n : p.getRequirements()) {
-                if (get(n) == null)
+                if (get(n) != null)
                     throw new IntegrityException("Dependency " + n +
                         " of plugin " + p.getName() + " absent");
+            }
+            for (String n : Util.concat(p.getAttr(Plugin.BEFORE),
+                                        p.getAttr(Plugin.DEPENDS))) {
+                if (get(n) == null) continue;
+                deps.add(get(n));
+            }
+            for (String n : p.getAttr(Plugin.AFTER)) {
+                Plugin o = get(n);
+                if (o == null) continue;
+                getDeps(ret, o).add(p);
             }
             for (String n : p.getAttr(Plugin.BREAKS)) {
                 if (get(n) != null)
@@ -92,6 +101,21 @@ public class PluginManager {
                         " conflicts with plugin " + n);
             }
         }
+        return ret;
+    }
+    private void traversePlugins(Plugin base, Map<Plugin, Set<Plugin>> deps,
+            Deque<Plugin> stack, Set<Plugin> drain)
+            throws IntegrityException {
+        if (base == stack.peekFirst())
+            throw new IntegrityException("Plugin " + base.getName() +
+                " is (transitively?) ordered before itself");
+        if (drain.contains(base))
+            return;
+        stack.add(base);
+        for (Plugin p : deps.get(base))
+            traversePlugins(p, deps, stack, drain);
+        drain.add(base);
+        stack.remove(base);
     }
 
 }
