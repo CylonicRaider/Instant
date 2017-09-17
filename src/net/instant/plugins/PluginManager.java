b4f8ea5986d5b42a559c67bd14d563d47dcab20d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-09-17 17:07:14 +0200

    [Backend] Implement plugin ordering computation

--- src/net/instant/plugins/PluginManager.java
+++ src/net/instant/plugins/PluginManager.java
@@ -1,15 +1,22 @@
 package net.instant.plugins;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class PluginManager {
 
     private final Map<String, Plugin> plugins;
     private PluginFetcher fetcher;
+    private List<Plugin> order;
 
     public PluginManager() {
         plugins = new LinkedHashMap<String, Plugin>();
@@ -40,6 +47,36 @@ public class PluginManager {
     }
     public void add(Plugin p) {
         plugins.put(p.getName(), p);
+        order = null;
+    }
+
+    public List<Plugin> getOrder() {
+        return order;
+    }
+    public List<Plugin> computeOrder() throws IntegrityException {
+        if (order == null) {
+            checkIntegrity();
+            Set<Plugin> tempOrder = new LinkedHashSet<Plugin>();
+            Deque<Plugin> stack = new LinkedList<Plugin>();
+            for (Plugin p : plugins.values())
+                traversePlugins(p, stack, tempOrder);
+            order = Collections.unmodifiableList(new ArrayList<Plugin>(
+                tempOrder));
+        }
+        return order;
+    }
+    private void traversePlugins(Plugin base, Deque<Plugin> stack,
+            Set<Plugin> drain) throws IntegrityException {
+        if (base == stack.peekFirst())
+            throw new IntegrityException("Plugin " + base.getName() +
+                " depends (transitively?) on itself");
+        if (drain.contains(base))
+            return;
+        stack.add(base);
+        for (String n : base.getRequirements())
+            traversePlugins(get(n), stack, drain);
+        drain.add(base);
+        stack.remove(base);
     }
 
     public void checkIntegrity() throws IntegrityException {
