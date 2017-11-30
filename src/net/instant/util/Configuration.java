6bfe5b92e012a30a083d1621d21bf58792e8e585
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-11-30 20:32:03 +0100

    [Backend] Commence more generic configuration management

---
+++ src/net/instant/util/Configuration.java
@@ -0,0 +1,54 @@
+package net.instant.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Configuration {
+
+    public interface DataSource {
+
+        String get(String key);
+
+    }
+
+    private final List<DataSource> sources;
+    private final Map<String, String> data;
+
+    public Configuration() {
+        sources = new ArrayList<DataSource>();
+        data = new HashMap<String, String>();
+    }
+
+    public String get(String key) {
+        if (data.containsKey(key)) return data.get(key);
+        String ret = null;
+        for (DataSource src : sources) {
+            ret = src.get(key);
+            if (ret != null) break;
+        }
+        data.put(key, ret);
+        return ret;
+    }
+
+    public String getRaw(String key) {
+        return data.get(key);
+    }
+
+    public void put(String key, String value) {
+        data.put(key, value);
+    }
+
+    public void remove(String key) {
+        data.remove(key);
+    }
+
+    public void addSource(DataSource source) {
+        sources.add(source);
+    }
+    public void removeSource(DataSource source) {
+        sources.remove(source);
+    }
+
+}
