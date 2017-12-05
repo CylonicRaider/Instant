798c7cadacc34bf759097dc921cc0bc3d088e995
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-12-05 20:23:38 +0100

    [Backend] Refactor backend plugin resources

--- src/net/instant/plugins/DefaultPlugin.java
+++ src/net/instant/plugins/DefaultPlugin.java
@@ -1,5 +1,7 @@
 package net.instant.plugins;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import net.instant.api.API1;
@@ -12,119 +14,45 @@ public class DefaultPlugin {
     public static final PluginAttribute<Boolean> FRONTEND_PLUGIN =
         new BooleanAttribute("Frontend-Plugin");
 
-    public enum PluginResource {
-
-        DEPS("deps") {
-
-            public final PluginAttribute<Set<String>> ATTR =
-                new StringSetAttribute("Frontend-Deps");
-
-            public String parse(PluginData data) {
-                return parseSet(data, ATTR);
-            }
-
-        },
-        STYLES("styles") {
-
-            public final PluginAttribute<Set<String>> ATTR =
-                new StringSetAttribute("Frontend-Styles");
-
-
-            public String parse(PluginData data) {
-                return parseSet(data, ATTR);
-            }
-
-        },
-        SCRIPTS("scripts") {
-
-            public final PluginAttribute<Set<String>> ATTR =
-                new StringSetAttribute("Frontend-Scripts");
-
-            public String parse(PluginData data) {
-                Set<String> urls = ATTR.get(data);
-                if (urls.isEmpty()) return null;
-                StringBuilder sb = new StringBuilder("[");
-                boolean first = true;
-                for (String url : urls) {
-                    if (first) {
-                        first = false;
-                    } else {
-                        sb.append(", ");
-                    }
-                    String[] parts = url.split("#", 2);
-                    sb.append("{url: ");
-                    sb.append(Util.escapeStringJS(parts[0], true));
-                    Map<String, String> params =
-                        Formats.parseQueryString((parts.length == 1) ? "" :
-                                                 parts[1]);
-                    for (String prop : new String[] { "before", "after",
-                                                      "isolate" }) {
-                        if (! Util.isTrue(params.get(prop))) continue;
-                        sb.append(", ");
-                        sb.append(prop);
-                        sb.append(": true");
-                    }
-                    sb.append("}");
-                }
-                return sb.append("]").toString();
-            }
-
-        },
-        LIBS("libs") {
-
-            public final PluginAttribute<Set<String>> ATTR =
-                new StringSetAttribute("Frontend-Libs");
-
-            public String parse(PluginData data) {
-                return parseSet(data, ATTR);
-            }
-
-        },
-        CODE("code") {
-
-            public final PluginAttribute<Set<String>> ATTR =
-                new StringSetAttribute("Frontend-Code");
-
-            public String parse(PluginData data) {
-                return parseSet(data, ATTR);
-            }
-
-        },
-        MAIN("main") {
-
-            public final PluginAttribute<String> ATTR =
-                new StringAttribute("Frontend-Main");
-
-            public String parse(PluginData data) {
-                String source = ATTR.get(data);
-                if (source == null) return null;
-                return "function() { " + source + "; }";
-            }
-
-        };
+    public static abstract class Resource<T> {
 
         private final String name;
+        private final PluginAttribute<T> attr;
 
-        private PluginResource(String name) {
+        public Resource(String name, PluginAttribute<T> attr) {
             this.name = name;
+            this.attr = attr;
         }
 
         public String getName() {
             return name;
         }
 
+        public PluginAttribute<T> getAttribute() {
+            return attr;
+        }
+
+        // Returns JS representation.
         public abstract String parse(PluginData data);
 
-        // HACK: Default implementation of parse().
-        // However semantically appropriate an enum is, some abstract classes
-        // would be handier...
-        private static String parseSet(PluginData data,
-                                       PluginAttribute<Set<String>> attr) {
-            Set<String> values = attr.get(data);
+    }
+
+    public static class StringSetResource extends Resource<Set<String>> {
+
+        public StringSetResource(String name,
+                                 PluginAttribute<Set<String>> attr) {
+            super(name, attr);
+        }
+        public StringSetResource(String name) {
+            super(name, new StringSetAttribute(name));
+        }
+
+        public String parse(PluginData data) {
+            Set<String> values = getAttribute().get(data);
             if (values.isEmpty()) return null;
             StringBuilder sb = new StringBuilder("[");
             boolean first = true;
-            for (String item : attr.get(data)) {
+            for (String item : values) {
                 if (first) {
                     first = false;
                 } else {
@@ -137,6 +65,70 @@ public class DefaultPlugin {
 
     }
 
+    public static final Resource<Set<String>> RES_DEPS =
+        new StringSetResource("Deps");
+    public static final Resource<Set<String>> RES_STYLES =
+        new StringSetResource("Styles");
+    public static final Resource<Set<String>> RES_SCRIPTS =
+            new StringSetResource("Scripts") {
+
+        public String parse(PluginData data) {
+            Set<String> urls = getAttribute().get(data);
+            if (urls.isEmpty()) return null;
+            StringBuilder sb = new StringBuilder("[");
+            boolean first = true;
+            for (String url : urls) {
+                if (first) {
+                    first = false;
+                } else {
+                    sb.append(", ");
+                }
+                String[] parts = url.split("#", 2);
+                sb.append("{url: ");
+                sb.append(Util.escapeStringJS(parts[0], true));
+                Map<String, String> params =
+                    Formats.parseQueryString((parts.length == 1) ? "" :
+                                             parts[1]);
+                for (String prop : new String[] { "before", "after",
+                                                  "isolate" }) {
+                    if (! Util.isTrue(params.get(prop))) continue;
+                    sb.append(", ");
+                    sb.append(prop);
+                    sb.append(": true");
+                }
+                sb.append("}");
+            }
+            return sb.append("]").toString();
+        }
+
+    };
+    public static final Resource<Set<String>> RES_LIBS =
+        new StringSetResource("Libs");
+    public static final Resource<Set<String>> RES_CODE =
+        new StringSetResource("Code");
+    public static final Resource<String> RES_MAIN = new Resource<String>(
+            "Main", new StringAttribute("Plugin-Main")) {
+
+        public String parse(PluginData data) {
+            String source = getAttribute().get(data);
+            if (source == null) return null;
+            return "function() { " + source + "; }";
+        }
+
+    };
+
+    public static final List<Resource<?>> RESOURCES;
+
+    static {
+        RESOURCES = new ArrayList<Resource<?>>();
+        RESOURCES.add(RES_DEPS);
+        RESOURCES.add(RES_STYLES);
+        RESOURCES.add(RES_SCRIPTS);
+        RESOURCES.add(RES_LIBS);
+        RESOURCES.add(RES_CODE);
+        RESOURCES.add(RES_MAIN);
+    }
+
     public static Object initInstantPlugin1(API1 api, PluginData data) {
         if (FRONTEND_PLUGIN.get(data)) initFrontendPlugin(api, data);
         return null;
@@ -147,7 +139,7 @@ public class DefaultPlugin {
         res.append(Util.escapeStringJS(data.getName(), true));
         res.append(", {");
         boolean first = true;
-        for (PluginResource r : PluginResource.values()) {
+        for (Resource<?> r : RESOURCES) {
             String s = r.parse(data);
             if (s == null) continue;
             if (first) {
