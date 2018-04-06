CylonicRaider/Instant

commit c81e16015a9a974d9f467ef4be89dd4d6d1d1df2

[Backend] Pass query strings into the file producer framework

src/net/instant/util/fileprod/QueryStrippingProducer.java
@@ -1,0 +1,44 @@
+package net.instant.util.fileprod;
+
+import java.io.IOException;
+import net.instant.util.Util;
+
+public class QueryStrippingProducer implements Producer {
+
+    public static class RenamedFileCell extends FileCell {
+
+        private final FileCell source;
+
+        public RenamedFileCell(String name, FileCell source) {
+            super(name, source.getData(), source.getCreated());
+            this.source = source;
+        }
+
+        public FileCell getSource() {
+            return source;
+        }
+
+    }
+
+    private final Producer inner;
+
+    public QueryStrippingProducer(Producer inner) {
+        this.inner = inner;
+    }
+
+    public Producer getInner() {
+        return inner;
+    }
+
+    public ProducerJob produce(String name) {
+        String lookupName = Util.splitQueryString(name)[0];
+        final ProducerJob job = inner.produce(lookupName);
+        if (job == null) return null;
+        return new ProducerJob(name) {
+            public FileCell produce() throws IOException {
+                return new RenamedFileCell(getName(), job.produce());
+            }
+        };
+    }
+
+}

src/net/instant/InstantRunner.java
@@ -44,6 +44,7 @@ import net.instant.util.fileprod.FileProducer;
 import net.instant.util.fileprod.ListProducer;
 import net.instant.util.fileprod.Producer;
 import net.instant.util.fileprod.ProducerJob;
+import net.instant.util.fileprod.QueryStrippingProducer;
 import net.instant.util.fileprod.StringProducer;
 import net.instant.util.stringmatch.DefaultStringMatcher;
 import net.instant.ws.InstantWebSocketServer;
@@ -239,8 +240,8 @@ public class InstantRunner implements API1 {
             files = new StaticFileHook(makeConfig(), new FileProducer());
             ListProducer l = files.getProducer().getProducer();
             l.add(makePluginFiles());
-            l.add(makeStringFiles());
-            l.add(makeSourceFiles());
+            l.add(new QueryStrippingProducer(makeStringFiles()));
+            l.add(new QueryStrippingProducer(makeSourceFiles()));
         }
         return files;
     }

src/net/instant/hooks/StaticFileHook.java
@@ -8,7 +8,6 @@ import net.instant.api.ClientConnection;
 import net.instant.api.RequestData;
 import net.instant.api.RequestType;
 import net.instant.api.ResponseBuilder;
-import net.instant.util.Util;
 import net.instant.util.config.Configuration;
 import net.instant.util.fileprod.FileCell;
 import net.instant.util.fileprod.FileProducer;
@@ -63,9 +62,8 @@ public class StaticFileHook extends HookAdapter {
         if (producer == null || req.getRequestType() != RequestType.HTTP ||
                 ! req.getMethod().equals("GET"))
             return false;
-        String rawPath = Util.splitQueryString(req.getPath())[0];
-        String path = aliases.match(rawPath);
-        if (path == null) path = rawPath;
+        String path = aliases.match(req.getPath());
+        if (path == null) path = req.getPath();
         FileCell ent;
         try {
             ent = producer.get(path);

src/net/instant/util/fileprod/FilesystemFileCell.java
@@ -1,26 +1,0 @@
-package net.instant.util.fileprod;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-public class FilesystemFileCell extends FileCell {
-
-    public File path;
-
-    public FilesystemFileCell(String name, File path) throws IOException {
-        super(name, new FileInputStream(path), path.lastModified());
-        this.path = path;
-    }
-
-    public File getPath() {
-        return path;
-    }
-
-    public boolean isValid() {
-        /* BUG: Assumes that no file was last modified at the Epoch */
-        long modified = getPath().lastModified();
-        return (modified != 0 && modified <= getCreated());
-    }
-
-}

src/net/instant/util/fileprod/FilesystemProducer.java
@@ -1,9 +1,32 @@
 package net.instant.util.fileprod;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FilesystemProducer implements Producer {
+
+    public static class FilesystemFileCell extends FileCell {
+
+        private final File path;
+
+        public FilesystemFileCell(String name, File path)
+                throws IOException {
+            super(name, new FileInputStream(path), path.lastModified());
+            this.path = path;
+        }
+
+        public File getPath() {
+            return path;
+        }
+
+        public boolean isValid() {
+            /* BUG: Assumes that no file was last modified at the Epoch */
+            long modified = getPath().lastModified();
+            return (modified != 0 && modified <= getCreated());
+        }
+
+    }
 
     private File chroot;
     private File workdir;

commit fbd1297e5eeeb5663f50117d0e01c6f78d0b6128

[Backend] Fix static file serving regression
Introduced by oversight in c81e16015a9a974d9f467ef4be89dd4d6d1d1df2.

src/net/instant/util/fileprod/FileCell.java
@@ -45,7 +45,7 @@ public class FileCell {
             try {
                 d = MessageDigest.getInstance("MD5");
             } catch (NoSuchAlgorithmException exc) {
-                // SRSLY?
+                // Seriously?
                 return null;
             }
             d.update(Encodings.toBytes(created));

src/net/instant/util/fileprod/QueryStrippingProducer.java
@@ -18,6 +18,10 @@ public class QueryStrippingProducer implements Producer {
             return source;
         }
 
+        public boolean isValid() {
+            return source.isValid();
+        }
+
     }
 
     private final Producer inner;