CylonicRaider/Instant

commit 560d630fc81810a292b5f0d0dc5dc5f3dcc12d9f

[Scripts] Commence log colorer
Also fixes some issues in the instabot log parsing code.

script/instabot.py
@@ -328,10 +328,11 @@ def log_exception(name, exc, trailer=None):
     if trailer is not None: msg += ' ' + trailer
     log(msg)
 
-LOGLINE_START = re.compile(r'^\[([0-9 Z:-]+)\]\s+([A-Z_-]+)\s+(.*)$')
+LOGLINE = re.compile(r'^\[([0-9 Z:-]+)\]\s+([A-Z0-9_-]+)(?:\s+(.*))?$')
 WHITESPACE = re.compile(r'\s+')
-SCALAR = re.compile(r'[^"\'\x28,\s]\S*|u?"(?:[^"\\]|\\.)*"|'
+SCALAR = re.compile(r'[^"\'(,\s][^\s),]+|u?"(?:[^"\\]|\\.)*"|'
     r'u?\'(?:[^\'\\]|\\.)*\'')
+TUPLE_ENTRY = re.compile(r'(%s)\s*(,)\s*' % SCALAR.pattern)
 TUPLE = re.compile(r'\(\s*(?:(?:%s)\s*,\s*)*(?:(?:%s)\s*)?\)' %
                    (SCALAR.pattern, SCALAR.pattern))
 EMPTY_TUPLE = re.compile(r'^\(\s*\)$')
@@ -343,37 +344,38 @@ CONSTANTS = {'None': None, 'True': True, 'False': False,
              'Ellipsis': Ellipsis}
 def read_logs(src, filt=None):
     for line in src:
-        m = LOGLINE_START.match(line)
+        m = LOGLINE.match(line)
         if not m: continue
-        ts, tag = m.group(1), m.group(2)
-        args, idx = m.group(3), 0
+        ts, tag, args = m.group(1, 2, 3)
         if filt and not filt(tag): continue
-        values, l = {}, len(args)
-        while idx < len(args):
-            m = WHITESPACE.match(args, idx)
-            if m:
+        values = {}
+        if args is not None:
+            idx = 0
+            while idx < len(args):
+                m = WHITESPACE.match(args, idx)
+                if m:
+                    idx = m.end()
+                    if idx == len(args): break
+                m = PARAM.match(args, idx)
+                if not m: break
                 idx = m.end()
-                continue
-            m = PARAM.match(args, idx)
-            if not m: break
-            idx = m.end()
-            name, val = m.group(1), m.group(2)
-            if val in CONSTANTS:
-                val = CONSTANTS[val]
-            elif INTEGER.match(val):
-                val = int(val)
-            elif val[0] in '\'"':
-                val = ast.literal_eval(val)
-            elif val[0] == '(':
-                if EMPTY_TUPLE.match(val):
-                    val = ()
-                elif TRAILING_COMMA.search(val):
+                name, val = m.group(1, 2)
+                if val in CONSTANTS:
+                    val = CONSTANTS[val]
+                elif INTEGER.match(val):
+                    val = int(val)
+                elif val[0] in '\'"':
                     val = ast.literal_eval(val)
-                else:
-                    val = ast.literal_eval('(' + val[1:-1] + ',)')
-            values[name] = val
-        else:
-            yield (ts, tag, values)
+                elif val[0] == '(':
+                    if EMPTY_TUPLE.match(val):
+                        val = ()
+                    elif TRAILING_COMMA.search(val):
+                        val = ast.literal_eval(val)
+                    else:
+                        val = ast.literal_eval('(' + val[1:-1] + ',)')
+                values[name] = val
+            if idx != len(args): continue
+        yield (ts, tag, values)
 
 class ArgParser:
     def __init__(self, args):

script/colorlogs.py
@@ -1,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Perform syntax highlighting on Scribe logs.
+
+import sys, os, re
+
+import instabot
+
+# Configurable constants
+DEFAULT_LOG_PATH = 'log/scribe.log'
+
+# Actual constants
+COLORS = {None: '\033[0m', 'bold': '\033[1m', 'black': '\033[30m',
+    'red': '\033[31m', 'green': '\033[32m', 'orange': '\033[33m',
+    'blue': '\033[34m', 'magenta': '\033[35m', 'cyan': '\033[36m',
+    'white': '\033[37m'}
+
+def highlight(line):
+    def highlight_scalar(val):
+        if val in instabot.CONSTANTS:
+            return (COLORS['magenta'], val)
+        elif instabot.INTEGER.match(val):
+            return (COLORS['cyan'], val)
+        else:
+            return (COLORS[None], val)
+    m = instabot.LOGLINE.match(line)
+    if not m: return line
+    ret = [line[:m.start(2)], COLORS['bold'], m.group(2), COLORS[None],
+           line[m.end(2):m.start(3)]]
+    idx = m.start(3)
+    if idx != -1:
+        while idx < len(line):
+            # Skip whitespace
+            m = instabot.WHITESPACE.match(line, idx)
+            if m:
+                ret.extend((COLORS[None], m.group()))
+                idx = m.end()
+                if idx == len(line): break
+            # Match the next parameter; output name
+            m = instabot.PARAM.match(line, idx)
+            if not m: break
+            name, val = m.group(1, 2)
+            ret.extend((COLORS['green'], name, '='))
+            # Output value
+            if val[0] == '(':
+                # Output tuple prefix
+                ret.extend((COLORS['orange'], '('))
+                sm = instabot.WHITESPACE.match(val, 1)
+                if sm:
+                    ret.extend((COLORS[None], sm.group()))
+                    subidx = sm.end()
+                else:
+                    subidx = 1
+                # Output tuple values
+                while subidx < len(val):
+                    sm = instabot.TUPLE_ENTRY.match(val, subidx)
+                    if not sm: break
+                    ret.extend(highlight_scalar(sm.group(1)))
+                    sp = val[sm.end(1):sm.start(2)]
+                    if sp: ret.extend((COLORS[None], sp))
+                    ret.extend((COLORS['orange'], ','))
+                    sp = val[sm.end(2):sm.end()]
+                    if sp: ret.extend((COLORS[None], sp))
+                    subidx = sm.end()
+                # Output trailing entry
+                sm = instabot.SCALAR.match(val, subidx)
+                if sm:
+                    ret.extend(highlight_scalar(sm.group()))
+                    ret.extend((COLORS[None], val[sm.end():-1]))
+                else:
+                    ret.extend((COLORS[None], val[subidx:-1]))
+                # Output suffix
+                ret.extend((COLORS['orange'], ')'))
+            else:
+                ret.extend(highlight_scalar(val))
+            idx = m.end()
+        ret.extend((COLORS[None], line[idx:]))
+    return ''.join(ret)
+
+def main():
+    for line in sys.stdin:
+        sys.stdout.write(highlight(line.rstrip('\n')) + '\n')
+        sys.stdout.flush()
+
+if __name__ == '__main__': main()

commit 09ea9e3bccb96c56f7f794822c5d514e9b286a61

[Instabot] Fix typo in log parsing code
Introduced by accident in 560d630fc81810a292b5f0d0dc5dc5f3dcc12d9f.

script/instabot.py
@@ -330,7 +330,7 @@ def log_exception(name, exc, trailer=None):
 
 LOGLINE = re.compile(r'^\[([0-9 Z:-]+)\]\s+([A-Z0-9_-]+)(?:\s+(.*))?$')
 WHITESPACE = re.compile(r'\s+')
-SCALAR = re.compile(r'[^"\'(,\s][^\s),]+|u?"(?:[^"\\]|\\.)*"|'
+SCALAR = re.compile(r'[^"\'(,\s][^\s),]*|u?"(?:[^"\\]|\\.)*"|'
     r'u?\'(?:[^\'\\]|\\.)*\'')
 TUPLE_ENTRY = re.compile(r'(%s)\s*(,)\s*' % SCALAR.pattern)
 TUPLE = re.compile(r'\(\s*(?:(?:%s)\s*,\s*)*(?:(?:%s)\s*)?\)' %