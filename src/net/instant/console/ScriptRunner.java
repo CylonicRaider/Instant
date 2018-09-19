596253d3cf9c004754d195955a9c89182f6e9f85
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-09-19 14:18:41 +0200

    [Backend] Add I/O utilities to ScriptRunner

--- src/net/instant/console/ScriptRunner.java
+++ src/net/instant/console/ScriptRunner.java
@@ -1,5 +1,7 @@
 package net.instant.console;
 
+import java.io.IOException;
+import java.io.Writer;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -32,6 +34,25 @@ public class ScriptRunner {
     public void setVariable(String name, Object value) {
         engine.getContext().setAttribute(name, value,
                                          ScriptContext.ENGINE_SCOPE);
+    }
+
+    public void redirectOutput(Writer output, Writer error) {
+        engine.getContext().setWriter(output);
+        engine.getContext().setErrorWriter(error);
+    }
+    public void redirectOutput(Writer output) {
+        redirectOutput(output, output);
+    }
+
+    public void print(String str) {
+        try {
+            engine.getContext().getWriter().write(str);
+        } catch (IOException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+    public void prinln(String str) {
+        print(str + "\n");
     }
 
     public Object execute(String script) throws ScriptException {
