0caab5a72992ef9af906b386af7913756217da3b
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-10-06 23:47:32 +0200

    [Scripts] Implement process starting and stopping API

--- script/run.py
+++ script/run.py
@@ -4,14 +4,17 @@
 # An init script for running Instant and a number of bots.
 
 import re
+import os, signal
 
 PID_LINE_RE = re.compile(r'^[0-9]+\s*$')
 
 class Process:
-    def __init__(self, name, pidfile):
+    def __init__(self, name, cmdline, pidfile):
         self.name = name
+        self.cmdline = cmdline
         self.pidfile = pidfile
         self._pid = Ellipsis
+        self._child = None
 
     def _read_pidfile(self):
         f = None
@@ -31,8 +34,15 @@ class Process:
             if f: f.close()
 
     def _write_pidfile(self, pid):
-        with open(self.pidfile, 'w') as f:
-            f.write('%s\n' % pid)
+        if pid is None:
+            try:
+                os.unlink(self.pidfile)
+            except OSError as e:
+                if e.errno == e.ENOENT: return
+                raise
+        else:
+            with open(self.pidfile, 'w') as f:
+                f.write('%s\n' % pid)
 
     def get_pid(self, force=False):
         if self._pid is Ellipsis or force:
@@ -42,6 +52,17 @@ class Process:
     def set_pid(self, pid):
         self._pid = pid
         self._write_pidfile(pid)
+
+    def start(self):
+        try:
+            cur_status = self.status()
+            if status == 'RUNNING':
+                return 'ALREADY_RUNNING'
+        except IOError:
+            pass
+        self._child = subprocess.Popen(self.cmdline, close_fds=False)
+        self.set_pid(self._child.pid)
+        return 'OK'
 
     def status(self):
         pid = self.get_pid()
@@ -53,6 +74,16 @@ class Process:
         except OSError as e:
             if e.errno == errno.ESRCH: return 'STALEFILE'
             raise
+
+    def stop(self):
+        if self._child is not None:
+            self._child.terminate()
+            self._child = None
+        else:
+            pid = self.get_pid()
+            if pid is not None:
+                os.kill(pid, signal.SIGTERM)
+        self.set_pid(None)
 
 def main():
     pass
