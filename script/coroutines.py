33ce8c19469b703b73727bd759b9655124919ae5
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-10-16 23:22:53 +0200

    [Scripts] Convert coroutine executor to OOP

--- script/coroutines.py
+++ script/coroutines.py
@@ -5,10 +5,25 @@
 
 import select
 
+class Executor:
+    def __init__(self):
+        self.routines = {}
+
+    def add(self, routine):
+        self.routines[routine] = True
+
+    def _remove(self, routine):
+        self.routines.pop(routine, None)
+
+    def __call__(self):
+        while self.routines:
+            for r in tuple(self.routines):
+                try:
+                    r.next()
+                except StopIteration:
+                    self._remove(r)
+
 def run(routines):
-    while routines:
-        for r in routines:
-            try:
-                result = r.next()
-            except StopIteration:
-                routines.remove(r)
+    ex = Executor()
+    for r in routines: ex.add(r)
+    r()
