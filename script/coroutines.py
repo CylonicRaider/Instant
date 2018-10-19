8ef061104c9067c42c2d8a5b06da4c9ae80da157
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-10-19 19:58:20 +0200

    [Scripts] Rework coroutine suspension

--- script/coroutines.py
+++ script/coroutines.py
@@ -6,49 +6,75 @@
 import select
 
 class Suspend:
-    def apply(self, executor, routine):
+    def apply(self, wake, executor, routine):
         raise NotImplementedError
+
+class Exit(Suspend):
+    def __init__(self, result):
+        self.result = result
+
+    def apply(self, wake, executor, routine):
+        routine.close()
+        executor._done(routine, self.result)
 
 class Call(Suspend):
     def __init__(self, target):
         self.target = target
 
-    def apply(self, executor, routine):
+    def apply(self, wake, executor, routine):
         executor.add(self.target)
-        executor.suspend(routine, self.target)
+        executor.listen(self.target, wake)
 
 class Executor:
     def __init__(self):
-        self.routines = set()
-        self.suspended = {}
+        self.routines = {}
+        self.suspended = set()
+        self.listening = {}
 
-    def add(self, routine):
-        self.routines.add(routine)
+    def add(self, routine, value=None):
+        self.routines[routine] = value
 
     def remove(self, routine):
-        self.routines.discard(routine)
+        self.suspended.discard(routine)
+        return self.routines.pop(routine, None)
 
-    def suspend(self, routine, reason):
-        self.routines.discard(routine)
-        self.suspended.setdefault(reason, []).append(routine)
+    def _suspend(self, routine):
+        if routine not in self.routines:
+            raise RuntimeError('Suspending not-running routine')
+        del self.routines[routine]
+        self.suspended.add(routine)
 
-    def wake(self, reason):
-        self.routines.update(self.suspended.pop(reason, ()))
+    def _wake(self, routine, value):
+        if routine in self.routines:
+            raise RuntimeError('Waking already-woken routine')
+        self.suspended.discard(routine)
+        self.routines[routine] = value
 
-    def _done(self, routine):
-        self.wake(routine)
+    def _done(self, routine, result=None):
+        self.trigger(routine, result)
         self.remove(routine)
 
+    def listen(self, event, callback):
+        self.listening.setdefault(event, []).append(callback)
+
+    def trigger(self, event, result=None):
+        for cb in self.listening.pop(event, ()):
+            cb(result)
+
     def __call__(self):
+        def make_wake(routine):
+            return lambda value=None: self._wake(routine, value)
         while self.routines or self.suspended:
-            for routine in tuple(self.routines):
+            for r, v in tuple(self.routines.items()):
+                self.routines[r] = None
                 try:
-                    suspend = routine.next()
+                    suspend = r.send(v)
                 except StopIteration:
-                    self._done(routine)
+                    self._done(r)
                     continue
-                if suspend is not None:
-                    suspend.apply(self, routine)
+                if suspend is None: continue
+                self._suspend(r)
+                suspend.apply(make_wake(r), self, r)
 
 def run(routines):
     ex = Executor()
