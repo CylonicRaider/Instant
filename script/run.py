CylonicRaider/Instant

commit 0d31b1d5bd400d1ed66ceffebb765f34c75e894f

[Scripts] Move run.py status logic into PIDFile

script/run.py
@@ -190,6 +190,21 @@ class PIDFile:
             self._pid = self._read_file()
         return self._pid
 
+    def get_status(self, force_read=False):
+        pid = self.get_pid(force=force_read)
+        if pid is None:
+            return (pid, 'NOT_RUNNING')
+        try:
+            os.kill(pid, 0)
+            status = 'RUNNING'
+        except OSError as e:
+            if e.errno == errno.ESRCH:
+                status = 'STALEFILE'
+            elif e.errno == errno.EPERM:
+                status = 'RUNNING_PRIVILEGED'
+            raise
+        return (pid, status)
+
     def set_pid(self, pid):
         self._pid = pid
         self._write_file(pid)
@@ -273,15 +288,8 @@ class Process:
 
     def status(self, verbose=True):
         exit = self._make_exit(None, verbose)
-        pid = self.pidfile.get_pid()
-        if pid is None:
-            yield exit('NOT_RUNNING')
-        try:
-            os.kill(pid, 0)
-            yield exit('RUNNING')
-        except OSError as e:
-            if e.errno == errno.ESRCH: yield exit('STALEFILE')
-            raise
+        pid, status = self.pidfile.get_status()
+        yield exit(status)
 
 class ProcessGroup:
     def __init__(self):

commit b390d66d960b54da3e0ec4989f9fb54dc2fa7fa3

[Scripts] Improve run.py stale PID file handling
Fixes a bug introduced in 0d31b1d5bd400d1ed66ceffebb765f34c75e894f.

script/run.py
@@ -263,10 +263,12 @@ class PIDFile:
             status = 'RUNNING'
         except OSError as e:
             if e.errno == errno.ESRCH:
-                status = 'STALEFILE'
+                pid = None
+                status = 'NOT_FOUND'
             elif e.errno == errno.EPERM:
                 status = 'RUNNING_PRIVILEGED'
-            raise
+            else:
+                raise
         return (pid, status)
 
     def set_pid(self, pid):
@@ -372,6 +374,7 @@ class Process:
     def stop(self, wait=True, verbose=False):
         exit = self._make_exit('stop', verbose)
         prev_child = self._child
+        status = 'OK'
         if self._child is not None:
             self._child.terminate()
             self._child = None
@@ -382,18 +385,20 @@ class Process:
             if pid is None:
                 yield exit('NOT_RUNNING')
             else:
-                os.kill(pid, signal.SIGTERM)
+                try:
+                    os.kill(pid, signal.SIGTERM)
+                except OSError as e:
+                    if e.errno != errno.ESRCH: raise
+                    status = 'NOT_FOUND'
         self.pidfile.set_pid(None)
         if wait:
             if prev_child:
                 raw_status = yield coroutines.WaitProcess(prev_child)
                 status = 'OK %s' % (raw_status,)
-            else:
+            elif status == 'OK':
                 delay = self.stop_delay
                 if delay > 0: yield coroutines.Sleep(delay)
                 status = None
-        else:
-            status = 'OK'
         yield exit(status)
 
     def status(self, verbose=False):