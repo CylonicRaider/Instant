CylonicRaider/Instant

commit 209573abc00b262ba5eda53f22875c58c7a098d8

[Scripts] Detect unknown process names

script/run.py
@@ -37,6 +37,13 @@ class RunnerError(Exception): pass
 
 class ConfigurationError(RunnerError): pass
 
+class NoSuchProcessesError(RunnerError):
+    def __init__(self, proclist):
+        RunnerError.__init__(self, 'Unknown process%s: %s' % (
+                             'es' if len(proclist) != 1 else '',
+                             ', '.join(map(str, sorted(proclist)))))
+        self.proclist = proclist
+
 class RemoteError(RunnerError):
     def __init__(self, code, message):
         RunnerError.__init__(self, '[%s] %s' % (code, message))
@@ -351,23 +358,34 @@ class ProcessGroup:
     def add(self, proc):
         self.processes.append(proc)
 
-    def _for_each(self, selector, handler):
-        calls = [coroutines.Call(handler(p))
-                 for p in filter(selector, self.processes)]
+    def _for_each(self, procs, handler):
+        if procs is None:
+            eff_procs = self.processes
+        else:
+            eff_procs = []
+            procs = set(procs)
+            for p in self.processes:
+                if p in procs or p.name in procs:
+                    eff_procs.append(p)
+                    procs.discard(p)
+                    procs.discard(p.name)
+            if procs:
+                raise NoSuchProcessesError(procs)
+        calls = [coroutines.Call(handler(p)) for p in eff_procs]
         result = yield coroutines.All(*calls)
         yield coroutines.Exit(result)
 
-    def warmup(self, wait=True, verbose=False, selector=None):
-        return self._for_each(selector, lambda p: p.warmup(wait, verbose))
+    def warmup(self, wait=True, verbose=False, procs=None):
+        return self._for_each(procs, lambda p: p.warmup(wait, verbose))
 
-    def start(self, wait=True, verbose=False, selector=None):
-        return self._for_each(selector, lambda p: p.start(wait, verbose))
+    def start(self, wait=True, verbose=False, procs=None):
+        return self._for_each(procs, lambda p: p.start(wait, verbose))
 
-    def stop(self, wait=True, verbose=False, selector=None):
-        return self._for_each(selector, lambda p: p.stop(wait, verbose))
+    def stop(self, wait=True, verbose=False, procs=None):
+        return self._for_each(procs, lambda p: p.stop(wait, verbose))
 
-    def status(self, verbose=False, selector=None):
-        return self._for_each(selector, lambda p: p.status(verbose))
+    def status(self, verbose=False, procs=None):
+        return self._for_each(procs, lambda p: p.status(verbose))
 
 OPERATIONS = {}
 def operation(**params):
@@ -463,54 +481,41 @@ class ProcessManager:
     def get_notify(self, key):
         return self.notifies.get(key)
 
-    def _process_selector(self, procs):
-        if not procs: return None
-        procs = frozenset(procs)
-        return lambda p: p.name in procs or p in procs
-
     @operation(wait=(bool, 'Whether to wait for the start\'s completion'),
                procs=(list, 'The processes to start (default: all)'))
     def do_start(self, wait=True, procs=None, verbose=False):
         "Start the given processes"
-        selector = self._process_selector(procs)
-        yield coroutines.Call(self.group.start(verbose=verbose,
-                                               selector=selector))
+        kwds = {'procs': procs, 'verbose': verbose}
+        yield coroutines.Call(self.group.start(**kwds))
 
     @operation(wait=(bool, 'Whether to wait for the stop\'s completion'),
                procs=(list, 'The processes to stop (default: all)'))
     def do_stop(self, wait=True, procs=None, verbose=False):
         "Stop the given processes"
-        selector = self._process_selector(procs)
-        yield coroutines.Call(self.group.stop(verbose=verbose,
-                                              selector=selector))
+        kwds = {'procs': procs, 'verbose': verbose}
+        yield coroutines.Call(self.group.stop(**kwds))
 
     @operation(procs=(list, 'The processes to restart (default: all)'))
     def do_restart(self, procs=None, verbose=False):
         "Restart the given processes"
-        selector = self._process_selector(procs)
-        yield coroutines.Call(self.group.stop(verbose=verbose,
-                                              selector=selector))
-        yield coroutines.Call(self.group.start(verbose=verbose,
-                                               selector=selector))
+        kwds = {'procs': procs, 'verbose': verbose}
+        yield coroutines.Call(self.group.stop(**kwds))
+        yield coroutines.Call(self.group.start(**kwds))
 
     @operation(procs=(list, 'The processes to restart (default: all)'))
     def do_bg_restart(self, procs=None, verbose=False):
         "Restart the given processes with pre-loading the new instances"
-        selector = self._process_selector(procs)
-        yield coroutines.Call(self.group.warmup(verbose=verbose,
-                                                selector=selector))
-        yield coroutines.Call(self.group.stop(verbose=verbose,
-                                              selector=selector))
-        yield coroutines.Call(self.group.start(verbose=verbose,
-                                               selector=selector))
+        kwds = {'procs': procs, 'verbose': verbose}
+        yield coroutines.Call(self.group.warmup(**kwds))
+        yield coroutines.Call(self.group.stop(**kwds))
+        yield coroutines.Call(self.group.start(**kwds))
 
 
     @operation(procs=(list, 'The processes to query (default: all)'))
     def do_status(self, procs=None, verbose=False):
         "Query the status of the given processes"
-        selector = self._process_selector(procs)
-        yield coroutines.Call(self.group.status(verbose=verbose,
-                                                selector=selector))
+        kwds = {'procs': procs, 'verbose': verbose}
+        yield coroutines.Call(self.group.status(**kwds))
 
 REMOTE_COMMANDS = {}
 def command(name, minargs=0, maxargs=None):
@@ -543,6 +548,8 @@ def _wrap_operation(opname, desc):
         self.parent.manager_logger.info('Doing ' + opname + '...')
         try:
             result = yield coroutines.Call(method(**kwds))
+        except NoSuchProcessesError as exc:
+            raise RemoteError('NXPROCS', str(exc))
         except RunnerError as exc:
             raise RemoteError('UNK', str(exc))
         else:

commit 085269817c8545cddf0d2d81f211bd5d805e4a29

[Scripts] Revise run.py argument composition
Fixes a bug introduced in 209573abc00b262ba5eda53f22875c58c7a098d8 and
undoes the decision made in d65ec4a57fb6d8c1c58a8e315344151b739603c2.
The actual remote management code conveniently omits arguments for
which no values were specified (such as empty lists), so that the point
made in d65ec4a is void.

script/run.py
@@ -1036,16 +1036,21 @@ def main():
             arguments.master = 'spawn'
     conn = try_call(do_connect, arguments.master,
                     conffile_path=arguments.config)
+    opname = arguments.cmd.replace('-', '_')
+    optypes = OPERATIONS[opname]['types']
     kwds = dict(arguments.__dict__)
-    for k in ('config', 'master', 'cmd'): del kwds[k]
+    for k in ('config', 'master', 'cmd'):
+        del kwds[k]
+    for k, v in tuple(kwds.items()):
+        if optypes[k] == list and not v:
+            del kwds[k]
     if conn is not None:
         conn.parent.prepare_executor().error_cb = coroutines_error_handler
         if arguments.cmd == 'cmd':
             cmdline = arguments.cmdline
         else:
-            opdesc = OPERATIONS[arguments.cmd.replace('-', '_')]
             cmdline = ([arguments.cmd.upper()] +
-                       mgr.compose_line(kwds, opdesc['types']))
+                       mgr.compose_line(kwds, optypes))
         try:
             try_call(run_client, conn, cmdline)
         finally:
@@ -1053,7 +1058,7 @@ def main():
             conn.close()
     else:
         kwds['verbose'] = True
-        func = getattr(mgr, 'do_' + arguments.cmd.replace('-', '_'))
+        func = getattr(mgr, 'do_' + opname)
         try_call(coroutines.run, [func(**kwds)], sigpipe=True,
                  on_error=coroutines_error_handler)
 