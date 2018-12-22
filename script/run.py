CylonicRaider/Instant

commit 085269817c8545cddf0d2d81f211bd5d805e4a29

[Scripts] Revise run.py argument composition
Fixes a bug introduced in 209573abc00b262ba5eda53f22875c58c7a098d8 and
undoes the decision made in d65ec4a57fb6d8c1c58a8e315344151b739603c2.
The actual remote management code conveniently omits arguments for
which no values were specified (such as empty lists), so that the point
made in d65ec4a is void.

script/run.py
@@ -1036,16 +1036,21 @@ def main():
             arguments.master = 'spawn'
     conn = try_call(do_connect, arguments.master,
                     conffile_path=arguments.config)
+    opname = arguments.cmd.replace('-', '_')
+    optypes = OPERATIONS[opname]['types']
     kwds = dict(arguments.__dict__)
-    for k in ('config', 'master', 'cmd'): del kwds[k]
+    for k in ('config', 'master', 'cmd'):
+        del kwds[k]
+    for k, v in tuple(kwds.items()):
+        if optypes[k] == list and not v:
+            del kwds[k]
     if conn is not None:
         conn.parent.prepare_executor().error_cb = coroutines_error_handler
         if arguments.cmd == 'cmd':
             cmdline = arguments.cmdline
         else:
-            opdesc = OPERATIONS[arguments.cmd.replace('-', '_')]
             cmdline = ([arguments.cmd.upper()] +
-                       mgr.compose_line(kwds, opdesc['types']))
+                       mgr.compose_line(kwds, optypes))
         try:
             try_call(run_client, conn, cmdline)
         finally:
@@ -1053,7 +1058,7 @@ def main():
             conn.close()
     else:
         kwds['verbose'] = True
-        func = getattr(mgr, 'do_' + arguments.cmd.replace('-', '_'))
+        func = getattr(mgr, 'do_' + opname)
         try_call(coroutines.run, [func(**kwds)], sigpipe=True,
                  on_error=coroutines_error_handler)
 

commit 1354ac41a358c5906f14073fb23a826ec5922a24

[Scripts] Make run.py cmd work again
Fixes a bug introduced in 085269817c8545cddf0d2d81f211bd5d805e4a29.

script/run.py
@@ -1086,14 +1086,15 @@ def main():
             arguments.master = 'spawn'
     conn = try_call(do_connect, arguments.master,
                     conffile_path=arguments.config)
-    opname = arguments.cmd.replace('-', '_')
-    optypes = OPERATIONS[opname]['types']
     kwds = dict(arguments.__dict__)
     for k in ('config', 'master', 'cmd'):
         del kwds[k]
-    for k, v in tuple(kwds.items()):
-        if optypes[k] == list and not v:
-            del kwds[k]
+    if arguments.cmd != 'cmd':
+        opname = arguments.cmd.replace('-', '_')
+        optypes = OPERATIONS[opname]['types']
+        for k, v in tuple(kwds.items()):
+            if optypes[k] == list and not v:
+                del kwds[k]
     if conn is not None:
         conn.parent.prepare_executor().error_cb = coroutines_error_handler
         if arguments.cmd == 'cmd':