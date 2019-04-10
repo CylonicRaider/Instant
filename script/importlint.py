CylonicRaider/Instant

commit 87cc4ac675abbc8a1c20399b740acd4ce34e5c41

[Makefile] [Linter] Add specific tokenizer

script/importlint.py
@@ -10,25 +10,25 @@ KEYWORDS = ('abstract|assert|boolean|break|byte|case|catch|char|class|const|'
     'goto|if|implements|import|instanceof|int|interface|long|native|new|'
     'package|private|protected|public|return|short|static|strictfp|super|'
     'switch|synchronized|this|throw|throws|transient|try|void|volatile|'
-    'while')
+    'while|'
+    # While not technically keywords, those are equivalent for our purposes.
+    'true|false|null')
 REGEXES = {
     # Identifier. Not matching the "e" in float literals.
     # FIXME: Unicode support.
-    'identifier': re.compile(r'(?!%s)(?<![a-zA-Z0-9_$])%s' %
+    'identifier': re.compile(r'(?<![a-zA-Z0-9_$])(?!%s)(?P<name>%s)' %
                              (KEYWORDS, DOT_IDENT)),
     # Interesting statement.
     'import': re.compile(r'(?<![a-zA-Z0-9_$])import\s+(static\s+)?'
-                         r'(?P<name>%s(\s*\.\s*\*));' % DOT_IDENT),
+                         r'(?P<name>%s(\s*\.\s*\*)?);' % DOT_IDENT),
     # Character or string. (The code is supposed to be syntactically valid.)
     'charstring': re.compile(r'''(?s)'([^']|\.)+'|"([^"]|\.)*"'''),
     # Comments.
     'linecomment': re.compile(r'(?m)//.*?$'),
     'blockcomment': re.compile(r'(?s)/\*.*?\*/'),
-    # Line separator.
-    'newline': re.compile(r'\n')
     }
 
-def regex_race(data, regexes):
+def match_tokens(data, regexes):
     """
     Regex race algorithm, as seen in the Instant frontend
     """
@@ -45,15 +45,43 @@ def regex_race(data, regexes):
         # Abort if no more matches.
         if not hits: break
         # Find first match.
-        first = min(hits.values(), key=lambda m: m.start())
+        firstname = min(hits, key=lambda k: hits[k].start())
+        first = hits[firstname]
         # Emit preceding data and the match.
         if first.start() != pos: yield data[pos:first.start()]
-        yield first
+        yield (firstname, first)
         # Prepare for next round.
         pos = first.end()
     # Emit remainder.
     if pos != len(data):
         yield data[pos:]
+
+def tokenize(data):
+    """
+    Partition a Java source file into a list of interesting and
+    non-interesting bits along with indexes of the formers
+    """
+    def normalize_tok_name(tok):
+        return re.sub(r'\s+', '', tok.group('name'))
+    lineno, ret, imports, idents = 1, [], [], []
+    for ent in match_tokens(data, REGEXES):
+        if isinstance(ent, str):
+            ret.append(ent)
+            # FIXME: Assuming no CR-only newlines...
+            lineno += ent.count('\n')
+            continue
+        name, tok = ent
+        if name == 'identifier':
+            # Indirect addressing to ease sorting imports below.
+            ret.append(('ident', len(idents), lineno))
+            idents.append((tok.group(), normalize_tok_name(tok)))
+        elif name == 'import':
+            ret.append(('import', len(imports), lineno))
+            imports.append((tok.group(), normalize_tok_name(tok)))
+        else:
+            ret.append(tok.group())
+        lineno += ent.count('\n')
+    return {None: ret, 'import': imports, 'ident': idents}
 
 def main():
     """

commit 9755ac4b6403eaa8724cc973157b7c3e5c8cd7c5

[Scripts] Fix importlint.py bug
Introduced by oversight in 87cc4ac675abbc8a1c20399b740acd4ce34e5c41,
the commit where the containing function was created.

script/importlint.py
@@ -101,7 +101,7 @@ def tokenize(data):
             package = normalize_name(tok.group('name'))
         else:
             ret.append(tok.group())
-        lineno += ent.count('\n')
+        lineno += tok.group().count('\n')
     return {None: ret, 'import': imports, 'ident': idents,
             'package': package}
 