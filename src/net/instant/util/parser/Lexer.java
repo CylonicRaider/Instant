efbfbd65c7d9a7fe65ef21e8638250e708858b3c
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-10-12 20:45:05 +0200

    [Backend] Store compiled Lexer production names separately
    Some of the names Grammar permits are not valid names for Java regular
    expression groups.

--- src/net/instant/util/parser/Lexer.java
+++ src/net/instant/util/parser/Lexer.java
@@ -1,5 +1,8 @@
 package net.instant.util.parser;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Lexer {
@@ -7,21 +10,32 @@ public class Lexer {
     public static class CompiledGrammar {
 
         private final Pattern pattern;
+        private final List<String> prodNames;
 
-        public CompiledGrammar(Pattern pattern) {
+        public CompiledGrammar(Pattern pattern, List<String> prodNames) {
             this.pattern = pattern;
+            this.prodNames = prodNames;
         }
 
         public Pattern getPattern() {
             return pattern;
         }
 
+        public List<String> getProductionNames() {
+            return prodNames;
+        }
+
     }
 
     private static void compileProductions(LexicalGrammar g,
-            String name, boolean top, StringBuilder sb) {
+            String name, boolean top, StringBuilder sb, List<String> names) {
         boolean first = true;
         for (Grammar.Production pr : g.getProductions(name)) {
+            if (top && pr.getSymbols().size() != 1)
+                throw new IllegalArgumentException(
+                    "Trying to compile a grammar with incorrect " +
+                    LexicalGrammar.LEXER_START_SYMBOL +
+                    " production symbol counts?!");
             if (first) {
                 first = false;
             } else {
@@ -33,13 +47,14 @@ public class Lexer {
                         throw new IllegalArgumentException(
                             "Trying to compile a grammar with invalid " +
                             LexicalGrammar.LEXER_START_SYMBOL +
-                            " productions");
-                    sb.append("(?<").append(sym.getContent()).append('>');
+                            " production symbols?!");
+                    names.add(pr.getName());
+                    sb.append('(');
                 } else {
                     sb.append("(?:");
                 }
                 if (sym.getType() == Grammar.SymbolType.NONTERMINAL) {
-                    compileProductions(g, sym.getContent(), false, sb);
+                    compileProductions(g, sym.getContent(), false, sb, null);
                 } else {
                     sb.append(sym.getPattern().pattern());
                 }
@@ -52,8 +67,12 @@ public class Lexer {
         LexicalGrammar cg = new LexicalGrammar(g);
         cg.validate();
         StringBuilder sb = new StringBuilder();
-        compileProductions(cg, LexicalGrammar.LEXER_START_SYMBOL, true, sb);
-        return new CompiledGrammar(Pattern.compile(sb.toString()));
+        ArrayList<String> prodNames = new ArrayList<String>();
+        compileProductions(cg, LexicalGrammar.LEXER_START_SYMBOL, true, sb,
+                           prodNames);
+        prodNames.trimToSize();
+        return new CompiledGrammar(Pattern.compile(sb.toString()),
+                                   Collections.unmodifiableList(prodNames));
     }
 
 }
