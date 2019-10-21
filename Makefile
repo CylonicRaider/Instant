CylonicRaider/Instant

commit 521d4810f73d2be182087eb57a838075f40c6ea0

[Makefile] Use partial Java builds for the backend

Makefile
@@ -25,8 +25,13 @@ Instant.jar: .build.jar $(LIBRARIES) $(ASSETS) $(AUTOASSETS)
 # Avoid recompiling the backend on frontend changes.
 .SECONDARY: .build.jar
 .build.jar: $(SOURCES)
+ifeq ($(strip $(MAKE_NO_PARTIAL_BUILDS)),)
+	@cd src && javac $(JAVACFLAGS) $$(../script/importlint.py --deps \
+	$(_JAVA_SOURCES) | ../script/jbuildcheck.py --cleanup --report)
+else
 	find src/net/ -name '*.class' -exec rm {} +
 	cd src && javac $(JAVACFLAGS) $(_JAVA_SOURCES)
+endif
 	cd src && jar cfe ../.build.jar Main $$(find . -name '*.class')
 
 Instant-run.jar: Instant.jar tools/amend-manifest.jar

commit 1b3d22b1f7f15fd7ffc02a37668084f84bde03a9

[Makefile] Delete obsolete class files
Fixes a bug introduced in 521d4810f73d2be182087eb57a838075f40c6ea0.

Makefile
@@ -27,7 +27,8 @@ Instant.jar: .build.jar $(LIBRARIES) $(ASSETS) $(AUTOASSETS)
 .build.jar: $(SOURCES)
 ifeq ($(strip $(MAKE_NO_PARTIAL_BUILDS)),)
 	@cd src && javac $(JAVACFLAGS) $$(../script/importlint.py --deps \
-	$(_JAVA_SOURCES) | ../script/jbuildcheck.py --cleanup --report)
+	$(_JAVA_SOURCES) | ../script/jbuildcheck.py --report \
+	--cleanup --cleandir src/net/)
 else
 	find src/net/ -name '*.class' -exec rm {} +
 	cd src && javac $(JAVACFLAGS) $(_JAVA_SOURCES)