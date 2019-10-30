4c7886682c99b45e2e81e61b48bedad651866a63
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-10-30 11:43:08 +0100

    [Backend] Move ParserGrammar into Parser
    Well, there is no parser, but we can move it there anyway.

---
+++ src/net/instant/util/parser/Parser.java
@@ -0,0 +1,38 @@
+package net.instant.util.parser;
+
+public class Parser {
+
+    public static class ParserGrammar extends Grammar {
+
+        public static final String START_SYMBOL = "$start";
+
+        private Grammar reference;
+
+        public ParserGrammar() {
+            super();
+        }
+        public ParserGrammar(GrammarView copyFrom) {
+            super(copyFrom);
+        }
+        public ParserGrammar(Production... productions) {
+            super(productions);
+        }
+
+        public Grammar getReference() {
+            return reference;
+        }
+        public void setReference(Grammar ref) {
+            reference = ref;
+        }
+
+        protected boolean checkProductions(String name) {
+            return (super.checkProductions(name) ||
+                (reference != null && reference.checkProductions(name)));
+        }
+        public void validate() throws InvalidGrammarException {
+            validate(START_SYMBOL);
+        }
+
+    }
+
+}
