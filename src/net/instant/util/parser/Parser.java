0272f843966b8edcb2390e699ebd915fbc9a98df
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-11-08 18:27:18 +0100

    [Backend] Commence parser implementation
    This adds basic scaffolding.

--- src/net/instant/util/parser/Parser.java
+++ src/net/instant/util/parser/Parser.java
@@ -1,5 +1,7 @@
 package net.instant.util.parser;
 
+import net.instant.util.LineColumnReader;
+
 public class Parser {
 
     public static class ParserGrammar extends Grammar {
@@ -31,6 +33,46 @@ public class Parser {
         }
         public void validate() throws InvalidGrammarException {
             validate(START_SYMBOL);
+        }
+
+    }
+
+    public interface Status {
+
+        Lexer.Token getCurrentToken();
+
+        void nextToken() throws ParsingException;
+
+        void setState(State next);
+
+        void pushState(State st, String treeNodeName);
+
+        void popState() throws ParsingException;
+
+    }
+
+    public interface State {
+
+        void apply(Status status) throws ParsingException;
+
+    }
+
+    public class ParsingException extends LocatedParserException {
+
+        public ParsingException(LineColumnReader.Coordinates pos) {
+            super(pos);
+        }
+        public ParsingException(LineColumnReader.Coordinates pos,
+                              String message) {
+            super(pos, message);
+        }
+        public ParsingException(LineColumnReader.Coordinates pos,
+                              Throwable cause) {
+            super(pos, cause);
+        }
+        public ParsingException(LineColumnReader.Coordinates pos,
+                              String message, Throwable cause) {
+            super(pos, message, cause);
         }
 
     }
