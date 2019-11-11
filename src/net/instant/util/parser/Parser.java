79536ccf7061fd6273979ed1b197f37ba9645ac0
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-11-11 08:54:45 +0100

    [Backend] Move parser implementation below interface

--- src/net/instant/util/parser/Parser.java
+++ src/net/instant/util/parser/Parser.java
@@ -40,6 +40,26 @@ public class Parser {
 
     }
 
+    public static class ParsingException extends LocatedParserException {
+
+        public ParsingException(LineColumnReader.Coordinates pos) {
+            super(pos);
+        }
+        public ParsingException(LineColumnReader.Coordinates pos,
+                              String message) {
+            super(pos, message);
+        }
+        public ParsingException(LineColumnReader.Coordinates pos,
+                              Throwable cause) {
+            super(pos, cause);
+        }
+        public ParsingException(LineColumnReader.Coordinates pos,
+                              String message, Throwable cause) {
+            super(pos, message, cause);
+        }
+
+    }
+
     public interface Status {
 
         LineColumnReader.Coordinates getCurrentPosition();
@@ -63,26 +83,6 @@ public class Parser {
     public interface State {
 
         void apply(Status status) throws ParsingException;
-
-    }
-
-    public static class ParsingException extends LocatedParserException {
-
-        public ParsingException(LineColumnReader.Coordinates pos) {
-            super(pos);
-        }
-        public ParsingException(LineColumnReader.Coordinates pos,
-                              String message) {
-            super(pos, message);
-        }
-        public ParsingException(LineColumnReader.Coordinates pos,
-                              Throwable cause) {
-            super(pos, cause);
-        }
-        public ParsingException(LineColumnReader.Coordinates pos,
-                              String message, Throwable cause) {
-            super(pos, message, cause);
-        }
 
     }
 
