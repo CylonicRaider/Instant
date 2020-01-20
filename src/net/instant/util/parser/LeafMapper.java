03085c58457d950530ae44d061e1bf3c6b3fafeb
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-01-20 23:19:28 +0100

    [Backend] Implement simple leaf mapper

---
+++ src/net/instant/util/parser/LeafMapper.java
@@ -0,0 +1,14 @@
+package net.instant.util.parser;
+
+public abstract class LeafMapper<T> implements Mapper<T> {
+
+    public T map(Parser.ParseTree pt) {
+        if (pt.childCount() > 0)
+            throw new IllegalArgumentException("Cannot map parse tree to " +
+                "object: Expected leaf, got non-leaf");
+        return mapInner(pt.getToken());
+    }
+
+    protected abstract T mapInner(Lexer.Token tok);
+
+}
