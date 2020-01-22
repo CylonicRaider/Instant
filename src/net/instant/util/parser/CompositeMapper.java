30b97770f5dc70325b51d349f8ef5bd666c2189d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-01-22 19:46:39 +0100

    [Backend] Add convenience functions for building mappers

--- src/net/instant/util/parser/CompositeMapper.java
+++ src/net/instant/util/parser/CompositeMapper.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public abstract class CompositeMapper<C, T> implements Mapper<T> {
 
+    public interface NodeMapper<C, T> {
+
+        T map(Lexer.Token tok, List<C> children);
+
+    }
+
     public T map(Parser.ParseTree pt) {
         List<C> children = new ArrayList<C>(pt.childCount());
         for (Parser.ParseTree t : pt.getChildren()) {
@@ -16,5 +22,20 @@ public abstract class CompositeMapper<C, T> implements Mapper<T> {
     protected abstract T mapInner(Lexer.Token tok, List<C> children);
 
     protected abstract C mapChild(Parser.ParseTree pt);
+
+    public static <C, T> CompositeMapper<C, T> of(
+            final NodeMapper<C, T> reduce, final Mapper<C> map) {
+        return new CompositeMapper<C, T>() {
+
+            protected T mapInner(Lexer.Token tok, List<C> children) {
+                return reduce.map(tok, children);
+            }
+
+            protected C mapChild(Parser.ParseTree pt) {
+                return map.map(pt);
+            }
+
+        };
+    }
 
 }
--- src/net/instant/util/parser/LeafMapper.java
+++ src/net/instant/util/parser/LeafMapper.java
@@ -2,6 +2,12 @@ package net.instant.util.parser;
 
 public abstract class LeafMapper<T> implements Mapper<T> {
 
+    public interface NodeMapper<T> {
+
+        T map(Lexer.Token tok);
+
+    }
+
     public T map(Parser.ParseTree pt) {
         if (pt.childCount() > 0)
             throw new IllegalArgumentException("Cannot map parse tree to " +
@@ -10,5 +16,13 @@ public abstract class LeafMapper<T> implements Mapper<T> {
     }
 
     protected abstract T mapInner(Lexer.Token tok);
+
+    public static <T> LeafMapper<T> of(final NodeMapper<T> wrap) {
+        return new LeafMapper<T>() {
+            protected T mapInner(Lexer.Token tok) {
+                return wrap.map(tok);
+            }
+        };
+    }
 
 }
