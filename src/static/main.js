CylonicRaider/Instant

commit 7cb287334ec3b7fdc814335400dda92fc33c0980

[Frontend] Make offscreen message processing more robust

src/static/main.js
@@ -1121,12 +1121,6 @@ this.Instant = function() {
                   });
                   /* Scroll back */
                   restore();
-                  /* Check for offscreen messages */
-                  Instant.animation.offscreen.update(added.map(
-                    function(key) {
-                      return Instant.message.get(key);
-                    }
-                  ));
                   /* Avoid stale node references */
                   Instant.animation.offscreen._updateMessages();
                   /* Detect earliest and latest message */
@@ -4458,7 +4452,9 @@ this.Instant = function() {
               var input = Instant.input.getNode();
               for (var i = 0; i < add.length; i++) {
                 var n = add[i];
-                if (n.classList.contains('offscreen')) continue;
+                if (! n.classList.contains('new') ||
+                    n.classList.contains('offscreen'))
+                  continue;
                 n.classList.add('offscreen');
                 var icmp = docCmp(n, input);
                 if (icmp < 0) {

commit 449cfe4fc41c135ae09058fdbe891c54c2d06657

[Frontend] Revise offscreen message handling
Now, the code tracks those messages that have the .offscreen class,
instead of both .new and .offscreen. In particular, this should
eliminate another class of messages sticking around in the unread
message preview area after having been on screen.

Also fixes a very old logic bug introduced in commit
7cb287334ec3b7fdc814335400dda92fc33c0980.

src/static/main.js
@@ -1193,7 +1193,7 @@ this.Instant = function() {
                   /* Prepare for scrolling */
                   var restore = Instant.input.saveScrollState(true);
                   /* Import messages */
-                  added.forEach(function(key) {
+                  var msgNodes = added.map(function(key) {
                     /* Sanitize input */
                     var msg = messages[key];
                     if (typeof msg.nick != 'string')
@@ -1201,12 +1201,13 @@ this.Instant = function() {
                     if (typeof msg.text != 'string')
                       msg.text = JSON.stringify(msg.text);
                     /* Import message */
-                    Instant.message.importMessage(msg, pane);
+                    return Instant.message.importMessage(msg, pane);
                   });
                   /* Scroll back */
                   restore();
                   /* Avoid stale node references */
                   Instant.animation.offscreen._updateMessages();
+                  Instant.animation.offscreen.checkMany(msgNodes);
                   /* Detect earliest and latest message */
                   data.data.forEach(function(el) {
                     if (! before || el.id < before) before = el.id;
@@ -6317,8 +6318,7 @@ this.Instant = function() {
         }
         /* Is the given message offscreen? */
         function isUnread(msg) {
-          return (msg.classList.contains('offscreen') &&
-                  msg.classList.contains('new'));
+          return msg.classList.contains('offscreen');
         }
         /* Is the given message offscreen and a mention of the current
          * user? */
@@ -6343,10 +6343,8 @@ this.Instant = function() {
             /* Link interesting node (lists) */
             messageBox = msgbox;
             containerNode = container;
-            unreadMessages = $clsAll('message new offscreen',
-                                     msgbox);
-            unreadMentions = $clsAll('message new offscreen ping',
-                                     msgbox);
+            unreadMessages = $clsAll('message offscreen', msgbox);
+            unreadMentions = $clsAll('message offscreen ping', msgbox);
             /* Extract the alerts themself */
             var aboveNode = $cls('alert-above', container);
             var belowNode = $cls('alert-below', container);
@@ -6465,8 +6463,7 @@ this.Instant = function() {
               var input = Instant.input.getNode();
               for (var i = 0; i < add.length; i++) {
                 var n = add[i];
-                if (! n.classList.contains('new') ||
-                    n.classList.contains('offscreen'))
+                if (n.classList.contains('offscreen'))
                   continue;
                 n.classList.add('offscreen');
                 var icmp = docCmp(n, input);