eb55c6f7a3ab72fbb0f2a73b824263a02bfb1cd6
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-10-15 22:20:12 +0200

    [Backend] Add string formatting to PEMObject

--- src/net/instant/ws/ssl/PEMObject.java
+++ src/net/instant/ws/ssl/PEMObject.java
@@ -1,6 +1,7 @@
 package net.instant.ws.ssl;
 
 import java.nio.ByteBuffer;
+import net.instant.util.Encodings;
 import net.instant.util.Util;
 
 public class PEMObject {
@@ -11,6 +12,23 @@ public class PEMObject {
     public PEMObject(String label, byte[] data) {
         this.label = label;
         this.data = ByteBuffer.wrap(data);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-----BEGIN");
+        if (! label.isEmpty()) sb.append(" ").append(label);
+        sb.append("-----\n");
+        String base64 = Encodings.toBase64(getDataBytes());
+        int len = base64.length();
+        for (int idx = 0; idx < len; idx += 64) {
+            sb.append(base64.substring(idx, Math.min(idx + 64, len)))
+                .append('\n');
+        }
+        sb.append("-----END");
+        if (! label.isEmpty()) sb.append(" ").append(label);
+        sb.append("-----");
+        return sb.toString();
     }
 
     public String getLabel() {
