CylonicRaider/Instant

commit 8d6194efab68a8947c2e445e28bc142daef473e4

[Frontend] Fix message text truncation bugs
The old algorithm would return two nodes corresponding to the top-level
node of the original, nested inside each other. That was not necessarily
bad, but undesirable at least.

src/static/main.js
@@ -2891,21 +2891,21 @@ this.Instant = function() {
            * actually took place, the return value gains a class of
            * copy-truncated. */
           truncatedCopy: function(content, maxChars) {
-            function traverse(src, dest) {
+            function traverse(src) {
+              var copy;
               if (src.nodeType == Node.TEXT_NODE) {
-                var remaining = maxChars - seen;
-                var add, truncated;
+                var remaining = maxChars - seenChars
                 if (remaining >= src.nodeValue.length) {
-                  add = src.cloneNode(false);
-                  truncated = false;
-                } else {
-                  add = $text(src.nodeValue.substring(0, remaining));
+                  copy = src.cloneNode(false);
+                } else if (remaining <= 0) {
                   truncated = true;
-                  ret.classList.add('copy-truncated')
+                  return null;
+                } else {
+                  copy = $text(src.nodeValue.substring(0, remaining));
+                  truncated = true;
                 }
-                seen += add.nodeValue.length;
-                dest.appendChild(add);
-                return truncated;
+                seenChars += copy.nodeValue.length;
+                return copy;
               } else if (src.nodeType != Node.ELEMENT_NODE) {
                 return;
               } else if (src.classList.contains('no-copy')) {
@@ -2913,16 +2913,20 @@ this.Instant = function() {
                 ret.classList.add('copy-reduced');
                 return;
               }
-              var copy = src.cloneNode(false);
-              dest.appendChild(copy);
-              return Array.prototype.some.call(src.childNodes, function(c) {
-                return traverse(c, copy);
-              });
+              copy = src.cloneNode(false);
+              var children = src.childNodes;
+              for (var i = 0; i < children.length; i++) {
+                var cp = traverse(children[i]);
+                if (cp == null) break;
+                copy.appendChild(cp);
+                if (truncated) break;
+              }
+              return copy;
             }
-            var ret = content.cloneNode(false);
+            var seenChars = 0, truncated = false;
+            var ret = traverse(content);
             ret.classList.add('copy');
-            var seen = 0;
-            traverse(content, ret);
+            if (truncated) ret.classList.add('copy-truncated');
             return ret;
           },
           /* Reverse the transformation performed by the parser */

commit df5837d965df64e5bf9a08d50c9c789cdfab9f37

[Frontend] Fix more message truncation bugs
Introduced by omission in 8d6194efab68a8947c2e445e28bc142daef473e4;
the code removed in the first diff hunk seems to have been passed
over entirely.

src/static/main.js
@@ -2991,9 +2991,8 @@ this.Instant = function() {
               } else if (src.nodeType != Node.ELEMENT_NODE) {
                 return;
               } else if (src.classList.contains('no-copy')) {
-                dest.appendChild($makeNode('span', 'copy-placeholder'));
-                ret.classList.add('copy-reduced');
-                return;
+                reduced = true;
+                return $makeNode('span', 'copy-placeholder');
               }
               copy = src.cloneNode(false);
               var children = src.childNodes;
@@ -3005,10 +3004,11 @@ this.Instant = function() {
               }
               return copy;
             }
-            var seenChars = 0, truncated = false;
+            var seenChars = 0, truncated = false, reduced = false;
             var ret = traverse(content);
             ret.classList.add('copy');
             if (truncated) ret.classList.add('copy-truncated');
+            if (reduced) ret.classList.add('copy-reduced');
             return ret;
           },
           /* Reverse the transformation performed by the parser */